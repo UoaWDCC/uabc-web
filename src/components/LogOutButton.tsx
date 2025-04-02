21aea9f43c1f2b93e01fe041346a82c3f5398047
taroj1205 <61367823+taroj1205@users.noreply.github.com>
2025-04-02 15:19:31 +1300

    Discard changes to src/components/LogOutButton.tsx

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,22 +1,22 @@
 'use client'
 
-import { memo, type PropsWithChildren } from 'react'
+import type { PropsWithChildren } from 'react'
 
 import type { ButtonProps } from './ui/button'
 import { Button } from './ui/button'
 
-export const LogOutButton = memo(
-  ({ children, size = 'icon', variant = 'ghost' }: PropsWithChildren<ButtonProps>) => (
-    <Button
-      size={size}
-      variant={variant}
-      onClick={() => {
-        throw new Error('Log Out Button Not Implemented')
-      }}
-    >
-      {children}
-    </Button>
-  ),
+export const LogOutButton = ({
+  children,
+  size = 'icon',
+  variant = 'ghost',
+}: PropsWithChildren<ButtonProps>) => (
+  <Button
+    size={size}
+    variant={variant}
+    onClick={() => {
+      throw new Error('Log Out Button Not Implemented')
+    }}
+  >
+    {children}
+  </Button>
 )
-
-LogOutButton.displayName = 'LogOutButton'
