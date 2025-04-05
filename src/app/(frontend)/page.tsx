a5e7fbd2a62ef6b95026d6d289c99ed8b9caa50b
taroj1205 <61367823+taroj1205@users.noreply.github.com>
2025-04-05 23:18:28 +1300

    refactor(home-page): rewrite tailwind homepage to Yamada UI (#34)

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,42 +1,107 @@
 import Link from 'next/link'
-
-import { Card } from '@/components/Card'
+import {
+  Card,
+  Button,
+  Container,
+  VStack,
+  Text,
+  Heading,
+  Link as UILink,
+  CardBody,
+  Center,
+  CardFooter,
+  Grid,
+  GridItem,
+} from '@yamada-ui/react'
 import { UabcLogo } from '@/components/UabcLogo'
-import { Button } from '@/components/ui/button'
 
 export default function HomePage() {
   return (
-    <div className="grid min-h-dvh place-items-center bg-card sm:bg-primary">
-      <Card
-        variant="card"
-        className="relative flex min-w-fit justify-between border-none p-6 sm:border sm:p-12"
-      >
-        <div className="flex max-w-[500px] flex-col justify-between gap-4 text-center">
-          <div className="flex flex-col gap-6">
-            <h1 className="text-4xl font-bold">UABC Booking Portal</h1>
-            <div className="grid place-items-center lg:hidden">
-              <UabcLogo size={200}></UabcLogo>
-            </div>
-            <p>🏸 Welcome to the UABC Booking Portal 🏸</p>
-            <p>
+    <Container
+      as={Grid}
+      minH="100dvh"
+      bg={{
+        base: 'primary',
+        sm: ['white', 'black'],
+      }}
+      justifyContent="center"
+      p={{ sm: '0' }}
+    >
+      <GridItem as={Center}>
+        <Card
+          bg={['white', 'black']}
+          variant="solid"
+          color={['black', 'white']}
+          w="fit-content"
+          textAlign="center"
+          flexDir="row"
+          p={{
+            base: '12',
+            sm: '6',
+          }}
+          gap="12"
+        >
+          <CardBody
+            as={VStack}
+            gap="6"
+            p="0"
+            maxW="500px"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Heading size="2xl" lineHeight="10">
+              UABC Booking Portal
+            </Heading>
+            <Center
+              display={{
+                base: 'none',
+                lg: 'grid',
+              }}
+              placeItems="center"
+            >
+              <UabcLogo size={200} />
+            </Center>
+            <Text>🏸 Welcome to the UABC Booking Portal 🏸</Text>
+            <Text>
               Easily book your badminton sessions with the University of Auckland Badminton Club.
-            </p>
-            <p>
+            </Text>
+            <Text>
               Enjoy quick, hassle-free reservations and get on the court in no time. Join our
               vibrant community and play your best game today!
-            </p>
-          </div>
-          <Button asChild size="lg">
-            <Link href="/sessions">Book a session!</Link>
-          </Button>
-          <Link href="/privacy" className="text-primary underline">
-            Privacy Policy
-          </Link>{' '}
-        </div>
-        <div className="ml-12 hidden place-items-center lg:grid">
-          <UabcLogo size={350}></UabcLogo>
-        </div>
-      </Card>
-    </div>
+            </Text>
+            <Button
+              as={Link}
+              size="lg"
+              w="full"
+              href="/sessions"
+              colorScheme="primary"
+              fontSize="sm"
+              fontWeight="normal"
+              lineHeight="5"
+              height="2.75rem"
+              _hover={{
+                bg: 'transparentize(primary, 90%)',
+              }}
+            >
+              Book a session!
+            </Button>
+            <UILink as={Link} href="/privacy" color="primary" textDecoration="underline">
+              Privacy Policy
+            </UILink>
+          </CardBody>
+          <CardFooter
+            display={{
+              base: 'grid',
+              lg: 'none',
+            }}
+            p="0"
+          >
+            <Center>
+              <UabcLogo size={350} />
+            </Center>
+          </CardFooter>
+        </Card>
+      </GridItem>
+    </Container>
   )
 }
