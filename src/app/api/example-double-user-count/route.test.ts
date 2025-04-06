4601c61befa6db69930f73ec928be3fccb279cb4
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-07 11:45:47 +1200

    refactor: move example route to /api

---
+++ src/app/api/example-double-user-count/route.test.ts
@@ -0,0 +1,57 @@
+import { describe, it, expect, afterEach } from 'vitest'
+
+import { GET } from '@/app/api/example-double-user-count/route'
+
+import dotenv from 'dotenv'
+import { clearCollection, testPayloadObject } from 'tests/utils'
+
+dotenv.config()
+
+describe('double user count', () => {
+  afterEach(async () => {
+    await clearCollection(testPayloadObject, 'user')
+  })
+
+  it('should double 1 user correctly', async () => {
+    await testPayloadObject.create({
+      collection: 'user',
+      data: {
+        firstName: 'ray',
+        lastName: 'zhao',
+        email: 'rayzhao@gmail.com',
+        password: '12132',
+        role: 'casual',
+      },
+    })
+
+    const res = await GET()
+    expect(await res.json()).toEqual(2)
+  })
+
+  it('should double 2 user correctly', async () => {
+    await testPayloadObject.create({
+      collection: 'user',
+      data: {
+        firstName: 'ray',
+        lastName: 'zhao',
+        email: 'rayzhao@gmail.com',
+        password: '12132',
+        role: 'casual',
+      },
+    })
+
+    await testPayloadObject.create({
+      collection: 'user',
+      data: {
+        firstName: 'straight',
+        lastName: 'zhao',
+        email: 'straight@gmail.com',
+        password: '12132',
+        role: 'casual',
+      },
+    })
+
+    const res = await GET()
+    expect(await res.json()).toEqual(4)
+  })
+})
