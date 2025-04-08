08a9c3c9bfcdc7b730819131390a7bc96f94133d
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-08 21:56:17 +1200

    refactor: use nextresponse instead of response

--- src/app/api/auth/google/callback/route.ts
+++ src/app/api/auth/google/callback/route.ts
@@ -25,12 +25,12 @@ export const GET = async (req: NextRequest) => {
   cookieStore.delete('state')
   // Check if Google code exists
   const code = params.get('code')
-  if (!code) return Response.json({ error: 'No code provided' }, { status: 400 })
+  if (!code) return NextResponse.json({ error: 'No code provided' }, { status: 400 })
   // Check if the scope matches
   const scope = params.get('scope')
   const scopes = scope?.split(' ')
   if (!scope || !scopes || googleAuthScopes.some((scope) => !scopes.includes(scope)))
-    return Response.json({ error: 'No scope or invalid scopes provided' }, { status: 400 })
+    return NextResponse.json({ error: 'No scope or invalid scopes provided' }, { status: 400 })
   // Fetch tokens from Google based on code received
   let tokens
   try {
@@ -38,10 +38,10 @@ export const GET = async (req: NextRequest) => {
     tokens = tokenFetchResponse.tokens
   } catch (error: unknown) {
     console.error(error)
-    return Response.json({ error: 'Error invalid google auth' }, { status: 500 })
+    return NextResponse.json({ error: 'Error invalid google auth' }, { status: 500 })
   }
   if (!tokens.access_token || !tokens.expiry_date || !tokens.id_token) {
-    return Response.json({ error: 'Error invalid google auth' }, { status: 500 })
+    return NextResponse.json({ error: 'Error invalid google auth' }, { status: 500 })
   }
   // Fetch user information with access_token
   const userInfoResponse = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
@@ -72,5 +72,5 @@ export const GET = async (req: NextRequest) => {
     idToken: tokens.id_token,
   })
 
-  return Response.json(newAuthData)
+  return NextResponse.json(newAuthData)
 }
