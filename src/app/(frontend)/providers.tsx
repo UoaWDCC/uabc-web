f4a699f0009cc8546183bfe5f00ef12e964466c6
Brandon Seng Han Chan <160544499+br-Chan@users.noreply.github.com>
2025-04-16 17:44:58 +1200

    Configure Yamada UI to use min-width media queries (#79)
    * chore: extend Yamada config to use min-width media queries.
    
    * fix: change responsive design props' order for min-width.
    
    * refactor: move custom ThemeConfig object to theme directory.

--- src/app/(frontend)/providers.tsx
+++ src/app/(frontend)/providers.tsx
@@ -1,11 +1,16 @@
 'use client'
-import { defaultTheme } from '@/theme'
-import { extendTheme, UIProvider } from '@yamada-ui/react'
+import { defaultConfig, defaultTheme } from '@/theme'
+import { extendConfig, extendTheme, UIProvider } from '@yamada-ui/react'
 import { FC, memo, PropsWithChildren } from 'react'
 
 export const Providers: FC<PropsWithChildren> = memo(({ children }) => {
+  const config = extendConfig(defaultConfig)
   const theme = extendTheme(defaultTheme)()
-  return <UIProvider theme={theme}>{children}</UIProvider>
+  return (
+    <UIProvider config={config} theme={theme}>
+      {children}
+    </UIProvider>
+  )
 })
 
 Providers.displayName = 'Providers'
