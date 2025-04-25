f42629aada2a5d58870ead5351c1f8826db526d9
taroj1205 <taroj1205@gmail.com>
2025-04-25 23:03:29 +1200

    chore: add JWT_SECRET to environment variables for enhanced security in frontend and update CD pipeline to include it in Fly.io secrets

--- apps/frontend/environment.d.ts
+++ apps/frontend/environment.d.ts
@@ -2,6 +2,12 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       /**
+       * Secret for signing JWT tokens and verifying their integrity/authenticity.
+       * View the [docs](https://github.com/UoaWDCC/uabc-web/wiki/Backend-Architecture) on more information on how to generate one.
+       */
+      JWT_SECRET: string
+
+      /**
        * The public URL of the frontend
        */
       NEXT_PUBLIC_URL: string
