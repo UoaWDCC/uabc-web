377467e36b52dda0244544ce4fdd2b4571a1a935
Derrick Trang <derrick.trang100@hotmail.com>
2025-04-30 21:41:44 +1200

    Changed Semester Request date time offset to true

--- apps/backend/src/types/request-models.ts/SemesterRequests.ts
+++ apps/backend/src/types/request-models.ts/SemesterRequests.ts
@@ -5,36 +5,44 @@ export const UpdateSemesterRequestBody = z.object({
   name: z.string().optional(),
   startDate: z
     .string()
-    .datetime({ message: "Invalid date format, should be in ISO 8601 format" })
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true })
     .optional(),
   endDate: z
     .string()
-    .datetime({ message: "Invalid date format, should be in ISO 8601 format" })
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true })
     .optional(),
   breakStart: z
     .string()
-    .datetime({ message: "Invalid date format, should be in ISO 8601 format" })
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true })
     .optional(),
   breakEnd: z
     .string()
-    .datetime({ message: "Invalid date format, should be in ISO 8601 format" })
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true })
     .optional(),
   bookingOpenDay: z.nativeEnum(Weekday).optional(),
   bookingOpenTime: z
     .string()
-    .datetime({ message: "Invalid date format, should be in ISO 8601 format" })
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true })
     .optional(),
 })
 
 export const CreateSemesterRequestBody = z.object({
   name: z.string(),
-  startDate: z.string().datetime({ message: "Invalid date format, should be in ISO 8601 format" }),
-  endDate: z.string().datetime({ message: "Invalid date format, should be in ISO 8601 format" }),
-  breakStart: z.string().datetime({ message: "Invalid date format, should be in ISO 8601 format" }),
+  startDate: z
+    .string()
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true }),
+  endDate: z
+    .string()
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true }),
+  breakStart: z
+    .string()
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true }),
 
-  breakEnd: z.string().datetime({ message: "Invalid date format, should be in ISO 8601 format" }),
+  breakEnd: z
+    .string()
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true }),
   bookingOpenDay: z.nativeEnum(Weekday),
   bookingOpenTime: z
     .string()
-    .datetime({ message: "Invalid date format, should be in ISO 8601 format" }),
+    .datetime({ message: "Invalid date format, should be in ISO 8601 format", offset: true }),
 })
