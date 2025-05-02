d3a5e1186e389e5427279f95cec7826a4e875011
mikais13 <mikai.somerville@gmail.com>
2025-05-03 02:12:10 +1200

    feat: write mocks for GameSessionSchedule, add CreateGameSessionScheduleData type

--- apps/backend/src/types/collections.ts
+++ apps/backend/src/types/collections.ts
@@ -1,4 +1,8 @@
-import type { Authentication, User } from "@/payload-types"
+import type { Authentication, GameSessionSchedule, User } from "@/payload-types"
 
 export type CreateAuthenticationData = Omit<Authentication, "id" | "createdAt" | "updatedAt">
 export type CreateUserData = Omit<User, "id" | "createdAt" | "updatedAt">
+export type CreateGameSessionScheduleData = Omit<
+  GameSessionSchedule,
+  "id" | "createdAt" | "updatedAt"
+>
