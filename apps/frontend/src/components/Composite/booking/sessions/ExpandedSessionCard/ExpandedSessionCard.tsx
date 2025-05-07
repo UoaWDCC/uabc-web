c06d308e35bf44b77c45adbc9f87616e584bedc1
mikais13 <mikai.somerville@gmail.com>
2025-05-07 20:59:51 +1200

    refactor: use CardBody and VStack components in ExpandedSessionCard

--- apps/frontend/src/components/Composite/booking/sessions/ExpandedSessionCard/ExpandedSessionCard.tsx
+++ apps/frontend/src/components/Composite/booking/sessions/ExpandedSessionCard/ExpandedSessionCard.tsx
@@ -1,5 +1,5 @@
 import type { CartGameSession } from "@/types/game-session"
-import { Box, Card, CardHeader, Text, VStack } from "@yamada-ui/react"
+import { Card, CardBody, CardHeader, Text, VStack } from "@yamada-ui/react"
 import { LevelSelector } from "./LevelSelector"
 interface ExpandedSessionCardProps {
   gameSession: CartGameSession
@@ -8,28 +8,31 @@ interface ExpandedSessionCardProps {
 export const ExpandedSessionCard = ({ gameSession }: ExpandedSessionCardProps) => (
   <Card fontSize="sm" rounded="md" shadow="none">
     <CardHeader backgroundColor="primary" paddingX={6} paddingY="md" roundedTop="md" shadow="lg">
-      <VStack gap="none">
+      <VStack gap="0">
         <Text color="white" fontSize="lg" fontWeight="medium">
           {gameSession.weekday}
         </Text>
-        <Text color="whiteAlpha.700" fontWeight="medium">
+        <Text color="whiteAlpha.800" fontWeight="medium">
           {gameSession.locationName}
         </Text>
       </VStack>
     </CardHeader>
-    <Box backgroundColor="gray.50" padding={6}>
-      <Text color="tertiary" fontWeight="semibold">
-        Address
-      </Text>
-      <Text color="tertiary.300">{gameSession.locationAddress}</Text>
-      <br />
-      <Text color="tertiary" fontWeight="semibold">
-        Time
-      </Text>
-      <Text casing="uppercase" color="tertiary.300">
-        {gameSession.startTime} - {gameSession.endTime}
-      </Text>
-    </Box>
+    <CardBody backgroundColor="gray.50" gap="sm">
+      <VStack gap="xs">
+        <Text color="tertiary" fontWeight="semibold">
+          Address
+        </Text>
+        <Text color="tertiary.400">{gameSession.locationAddress}</Text>
+      </VStack>
+      <VStack gap="xs">
+        <Text color="tertiary" fontWeight="semibold">
+          Time
+        </Text>
+        <Text casing="uppercase" color="tertiary.400">
+          {gameSession.startTime} - {gameSession.endTime}
+        </Text>
+      </VStack>
+    </CardBody>
     <LevelSelector id={gameSession.id} selectedLevel={gameSession.playLevel} />
   </Card>
 )
