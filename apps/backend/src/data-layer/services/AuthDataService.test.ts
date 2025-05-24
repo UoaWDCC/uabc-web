efb7c8eae6b3af3755f0f4cef0117638795aaeef
r1zyn <ascendus.dev@gmail.com>
2025-05-24 13:00:56 +1200

    refactor: removed clear collection from tests

--- apps/backend/src/data-layer/services/AuthDataService.test.ts
+++ apps/backend/src/data-layer/services/AuthDataService.test.ts
@@ -1,26 +1,17 @@
 import { payload } from "@/data-layer/adapters/Payload"
-import { clearCollection } from "@/test-config/backend-utils"
 import {
   googleAuthCreateMock,
   standardAuthCreateMock,
 } from "@/test-config/mocks/Authentication.mock"
 import { userMock } from "@/test-config/mocks/User.mock"
-import dotenv from "dotenv"
+import "dotenv/config"
 import AuthDataService from "./AuthDataService"
 import UserDataService from "./UserDataService"
 
 const authDataService = new AuthDataService()
 const userDataService = new UserDataService()
 
-dotenv.config()
-
 describe("auth service", () => {
-  afterEach(async () => {
-    await clearCollection(payload, "authentication")
-    await clearCollection(payload, "user")
-    vi.restoreAllMocks()
-  })
-
   it("should create an authentication document for google auth", async () => {
     await userDataService.createUser(userMock)
 
