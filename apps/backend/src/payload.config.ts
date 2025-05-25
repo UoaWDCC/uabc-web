6bf4d2c0be64e231a00f6300756ff9c503deff9a
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-05-26 00:57:10 +1200

    chore: remove unused dependencies from backend (#229)
    * chore: remove unused dependencies from backend
    
    Removed `date-fns` and `date-fns-tz`
    
    * chore: remove payload cloud plugin
    
    * chore: disable graphql endpoint
    
    * chore: update pnpm lock
    
    * refactor: remove unused graphql routes
    
    * fix: move "use server" directive inside function
    
    * build: update payload and dependencies to 3.39.1

--- apps/backend/src/payload.config.ts
+++ apps/backend/src/payload.config.ts
@@ -2,7 +2,6 @@ import path from "node:path"
 import { fileURLToPath } from "node:url"
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from "@payloadcms/db-mongodb"
-import { payloadCloudPlugin } from "@payloadcms/payload-cloud"
 import { lexicalEditor } from "@payloadcms/richtext-lexical"
 import { buildConfig } from "payload"
 import sharp from "sharp"
@@ -28,8 +27,6 @@ export default buildConfig({
   routes: {
     admin: "/payload/admin",
     api: "/payload/api",
-    graphQL: "/payload/graphql",
-    graphQLPlayground: "/payload/graphql-playground",
   },
   admin: {
     user: Admin.slug,
@@ -54,12 +51,14 @@ export default buildConfig({
     outputFile: path.resolve(dirname, "../../../packages/shared/src/payload-types.ts"),
     declare: false,
   },
+  graphQL: {
+    disable: true,
+  },
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || "",
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
 })
