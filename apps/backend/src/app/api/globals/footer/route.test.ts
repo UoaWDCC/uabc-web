74995842dceb27e942547cbf619aa711f73ba370
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-05-26 15:56:04 +1200

    feat(globals): adds api endpoint for footer global

---
+++ apps/backend/src/app/api/globals/footer/route.test.ts
@@ -0,0 +1,29 @@
+import { GET } from "./route"
+
+describe("globals/footer", () => {
+  describe("GET", () => {
+    it("should return a global footer", async () => {
+      const response = await GET()
+      const json = await response.json()
+
+      expect(json).toStrictEqual({
+        data: {
+          title: "UABC",
+          description: "The largest university badminton club in New Zealand!",
+          facebook: "https://www.facebook.com/groups/uoabadminton/",
+          instagram: "https://www.instagram.com/uoa.badminton/?hl=en",
+          linkGroup1: {
+            title: "Quick Links",
+            links: [],
+          },
+          linkGroup2: {
+            title: "UABC",
+            links: [],
+          },
+          copyright: "© 2025 University of Auckland Badminton Club. \nAll rights reserved.",
+          credits: "Developed by the 2025 WDCC UABC Team.",
+        },
+      })
+    })
+  })
+})
