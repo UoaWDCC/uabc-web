0220b2f5d79dd4c4042e8a96ab2d701852b343ae
Shintaro Jokagi <61367823+taroj1205@users.noreply.github.com>
2025-05-28 10:27:24 +1200

    feat(theme): integrate initial color mode configuration and remove global styles (#249)
    * feat(theme): integrate initial color mode configuration and remove global styles
    
    * feat(fonts): update font family from Inter to Geist in Storybook configuration

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { UIProvider } from "@repo/ui/providers"
+import { ColorModeScript } from "@yamada-ui/react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +24,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
+        <ColorModeScript initialColorMode="dark" />
         <UIProvider>{children}</UIProvider>
       </body>
     </html>
