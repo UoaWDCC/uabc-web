f96ca04efa66f13ffdf2375657f8e9734dff1ec4
taroj1205 <taroj1205@gmail.com>
2025-05-27 15:51:41 +1200

    feat(storybook): add custom styles for fullscreen layout and import styles.css

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -10,6 +10,8 @@ import { useEffect, useState } from "react"
 import { DARK_MODE_EVENT_NAME } from "storybook-dark-mode"
 import { customThemes } from "./themes"
 
+import "./styles.css"
+
 const channel = addons.getChannel()
 
 const useDarkMode = (callback?: (darkMode: boolean) => void) => {
