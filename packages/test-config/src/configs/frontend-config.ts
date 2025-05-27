3a0322f85fd62583e1a3dafef324fd8a12df6328
Shintaro Jokagi <61367823+taroj1205@users.noreply.github.com>
2025-05-27 17:42:08 +1200

    feat(yamada): add `yamada-ui` and `@repo/ui` to frontend (#246)

--- packages/test-config/src/configs/frontend-config.ts
+++ packages/test-config/src/configs/frontend-config.ts
@@ -5,5 +5,13 @@ export const frontendConfig = mergeConfig(baseConfig, {
   test: {
     environment: "jsdom",
     setupFiles: ["@repo/test-config/setups/dom"],
+    coverage: {
+      thresholds: {
+        branches: 0, // TODO: Set appropriate thresholds
+        functions: 0,
+        lines: 0,
+        statements: 0,
+      },
+    },
   },
 })
