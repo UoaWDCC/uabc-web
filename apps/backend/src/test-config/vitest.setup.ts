80c423c3691b805564e2efb7a5e58b518ecafbc9
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-05-29 14:33:57 +1200

    feat(test): seeds users for each test execution

--- apps/backend/src/test-config/vitest.setup.ts
+++ apps/backend/src/test-config/vitest.setup.ts
@@ -2,7 +2,6 @@ import type { CollectionSlug } from "payload"
 
 import AuthService from "@/business-layer/services/AuthService"
 import { payload } from "@/data-layer/adapters/Payload"
-import UserDataService from "@/data-layer/services/UserDataService"
 import { clearCollection } from "./backend-utils"
 import { JWT_SECRET_MOCK } from "./mocks/GoogleAuth.mock"
 import { adminUserMock, casualUserMock, memberUserMock } from "./mocks/User.mock"
@@ -14,39 +13,10 @@ let casualToken: string
 let memberToken: string
 let adminToken: string
 
-const userDataService = new UserDataService()
 const authService = new AuthService()
 
 beforeEach(async () => {
   process.env.JWT_SECRET = JWT_SECRET_MOCK
-
-  // vi.mock("@/business-layer/services/AuthService", async () => {
-  //   return {
-  //     default: {
-  //       signJWT: vi.fn().mockImplementation((payload: JWTEncryptedUser, _options) => {
-  //         if (payload.user.email === adminUserMock.email) return ADMIN_JWT_MOCK
-  //         if (payload.user.email === memberUserMock.email) return MEMBER_JWT_MOCK
-  //         if (payload.user.email === casualUserMock.email) return CASUAL_JWT_MOCK
-  //         return JWT_TOKEN_MOCK
-  //       }),
-  //       decodeJWT: vi.fn().mockImplementation((token): JwtPayload | string => {
-  //         if (token === ADMIN_JWT_MOCK) return { user: adminUserMock }
-  //         if (token === MEMBER_JWT_MOCK) return { user: memberUserMock }
-  //         if (token === CASUAL_JWT_MOCK) return { user: casualUserMock }
-  //         return {}
-  //       }),
-  //       getData: vi
-  //         .fn()
-  //         .mockImplementation(
-  //           <T extends z.ZodTypeAny>(token: string, schema: T): z.infer<T> | undefined => {
-  //             if (token === ADMIN_JWT_MOCK) return { user: adminUserMock }
-  //             if (token === MEMBER_JWT_MOCK) return { user: memberUserMock }
-  //             if (token === CASUAL_JWT_MOCK) return { user: casualUserMock }
-  //           },
-  //         ),
-  //     },
-  //   }
-  // })
 
   vi.mock("next/headers", () => ({
     cookies: vi.fn(() => ({
@@ -69,7 +39,10 @@ beforeEach(async () => {
 
   const usersToCreate = [casualUserMock, memberUserMock, adminUserMock]
   for (const user of usersToCreate) {
-    await userDataService.createUser(user)
+    await payload.create({
+      collection: "user",
+      data: user,
+    })
   }
 
   casualToken = authService.signJWT({ user: casualUserMock })
