8c817281246f578dff4a004fda5f1985eaa1e3d5
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-06-04 01:42:15 +1200

    chore(storybook): remove addon-controls

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
   stories: ["../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-controls",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "storybook-dark-mode",
-  ],
+  addons: ["@storybook/addon-docs", "@storybook/addon-a11y", "storybook-dark-mode"],
   core: { disableTelemetry: true },
   framework: "@storybook/nextjs",
   staticDirs: ["../../../apps/frontend/public"],
