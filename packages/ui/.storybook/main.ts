a481fa89cbfdd8318703086e8cf23d3f6cfe7845
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-06-04 00:38:14 +1200

    chore(storybook): update storybook addons

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -3,13 +3,8 @@ import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
   stories: ["../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    /**
-     * Note for reviewer: `@storybook/addon-essentials` has been removed and it's functionality
-     * is now provided by individual addons like `@storybook/addon-actions`, `@storybook/addon-links`, etc.
-     */
     "@storybook/addon-viewport",
     "@storybook/addon-controls",
-    "@storybook/addon-interactions",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-backgrounds",
