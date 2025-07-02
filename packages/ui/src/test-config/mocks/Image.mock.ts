b2bbaf90bf5a39df3575352ba0c57dccf078d738
mikais13 <mikai.somerville@gmail.com>
2025-07-02 16:49:33 +1200

    refactor(location-bubble): move LocationBubble mocks to own file

--- packages/ui/src/test-config/mocks/Image.mock.ts
+++ packages/ui/src/test-config/mocks/Image.mock.ts
@@ -5,8 +5,3 @@ export const IMAGE_TEST_CONSTANTS = {
   height: 100,
   "data-testid": "next-image",
 }
-
-export const LOCATION_BUBBLE_TEST_CONSTANTS = {
-  locationImage: "https://placehold.co/300x200/png",
-  locationTitle: "Uoa Recreation Centre",
-}
---
+++ packages/ui/src/test-config/mocks/LocationBubble.mock.ts
@@ -0,0 +1,4 @@
+export const LOCATION_BUBBLE_TEST_CONSTANTS = {
+  locationImage: "https://placehold.co/300x200/png",
+  locationTitle: "Uoa Recreation Centre",
+}
