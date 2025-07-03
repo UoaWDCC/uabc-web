bcf44d557f4bfd120d235d4a9ce001aa4d300ce4
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-07-04 00:46:49 +1200

    test(storybook): create booking times card stories

---
+++ packages/ui/src/components/Composite/BookingTimesCard/BookingTimesCard.stories.tsx
@@ -0,0 +1,78 @@
+import type { Meta, StoryFn } from "@storybook/nextjs"
+import { PropsTable } from "@storybook-config/components"
+import { BookingTimesCard } from "./BookingTimesCard"
+import { styles } from "./BookingTimesCard.style"
+
+const meta: Meta<typeof BookingTimesCard> = {
+  title: "Composite Components / BookingTimesCard",
+  component: BookingTimesCard,
+  argTypes: {
+    title: {
+      control: "text",
+      description: "The components heading title",
+      table: {
+        type: { summary: "string" },
+        defaultValue: { summary: "Tuesday, 12th May" },
+      },
+    },
+    bookingTime: {
+      control: "text",
+      description: "The components booking time",
+      table: {
+        type: { summary: "string" },
+        defaultValue: { summary: "7:30 - 10pm" },
+      },
+    },
+    location: {
+      control: "text",
+      description: "The components location",
+      table: {
+        type: { summary: "string" },
+        defaultValue: { summary: "UoA Hiwa Center" },
+      },
+    },
+    type: {
+      control: "select",
+      options: ["default", "selected", "full"],
+      description: "The components type",
+      table: {
+        type: { summary: '"default" | "selected" | "full"' },
+        defaultValue: { summary: "default" },
+      },
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryFn<typeof BookingTimesCard>
+
+export const Default: Story = (args) => {
+  return (
+    <BookingTimesCard
+      {...args}
+      bookingTime="7:30 - 10pm"
+      location="UoA Hiwa Center"
+      title="Tuesday, 12th May"
+    />
+  )
+}
+
+export const Types: Story = (args) => {
+  return (
+    <PropsTable columns={["Types"]} rows={Object.keys(styles)}>
+      {(_col, row, key) => {
+        return (
+          <BookingTimesCard
+            key={key}
+            {...args}
+            bookingTime="7:30 - 10pm"
+            location="UoA Hiwa Center"
+            title="Tuesday, 12th May"
+            type={row}
+          />
+        )
+      }}
+    </PropsTable>
+  )
+}
---
+++ packages/ui/src/components/Composite/BookingTimesCard/BookingTimesCard.style.ts
@@ -0,0 +1,16 @@
+import type { CardProps } from "@yamada-ui/react"
+
+export const styles: Record<string, CardProps> = {
+  default: {
+    bg: ["secondary.50", "secondary.900"],
+    borderWidth: "1px",
+  },
+  selected: {
+    bg: ["primary.50", "secondary.950"],
+    borderColor: "secondary.200",
+    borderWidth: "3px",
+  },
+  full: {
+    bg: ["gray.100", "gray.800"],
+  },
+}
