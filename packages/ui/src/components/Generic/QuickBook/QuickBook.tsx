4a9b7d1dbf48bd2422c187a4e4a97e040706265a
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-06 17:12:02 +1200

    refactor: add memo() to quick book component

--- packages/ui/src/components/Generic/QuickBook/QuickBook.tsx
+++ packages/ui/src/components/Generic/QuickBook/QuickBook.tsx
@@ -1,7 +1,7 @@
 import { PlayLevel } from "@repo/shared"
 import { Button, Heading, Select } from "@repo/ui/components/Primitive"
 import { CalendarClockIcon, CircleGaugeIcon } from "@yamada-ui/lucide"
-import { Option, Stack, VStack } from "@yamada-ui/react"
+import { memo, Option, Stack, VStack } from "@yamada-ui/react"
 
 export interface QuickBookProps {
   title?: string
@@ -9,38 +9,36 @@ export interface QuickBookProps {
   schedules: { id: string; label: string }[]
 }
 
-export const QuickBook = ({
-  title = "Quick Book",
-  submitLabel = "Book Now",
-  schedules,
-}: QuickBookProps) => {
-  return (
-    <VStack bgColor="secondary.800" borderRadius="3xl" borderWidth="medium" p="md">
-      <Heading.h2>{title}</Heading.h2>
-      <Stack direction={{ base: "column", md: "row" }}>
-        <Select icon={<CalendarClockIcon fontSize={24} />} label="Location & Time" stylised>
-          {schedules.map((schedule) => (
-            <Option key={schedule.id} value={schedule.label}>
-              {schedule.label}
-            </Option>
-          ))}
-        </Select>
-        <Select icon={<CircleGaugeIcon fontSize={24} />} label="Skill Level" stylised>
-          {Object.keys(PlayLevel).map((playLevel) => {
-            const formattedPlayLevel = playLevel.charAt(0).toUpperCase() + playLevel.slice(1)
-            return (
-              <Option key={playLevel} value={playLevel}>
-                {formattedPlayLevel}
+export const QuickBook = memo(
+  ({ title = "Quick Book", submitLabel = "Book Now", schedules }: QuickBookProps) => {
+    return (
+      <VStack bgColor="secondary.800" borderRadius="3xl" borderWidth="medium" p="md">
+        <Heading.h2>{title}</Heading.h2>
+        <Stack direction={{ base: "column", md: "row" }}>
+          <Select icon={<CalendarClockIcon fontSize={24} />} label="Location & Time" stylised>
+            {schedules.map((schedule) => (
+              <Option key={schedule.id} value={schedule.label}>
+                {schedule.label}
               </Option>
-            )
-          })}
-        </Select>
-        <Button colorScheme="primary" size="lg" variant="gradient">
-          {submitLabel}
-        </Button>
-      </Stack>
-    </VStack>
-  )
-}
+            ))}
+          </Select>
+          <Select icon={<CircleGaugeIcon fontSize={24} />} label="Skill Level" stylised>
+            {Object.keys(PlayLevel).map((playLevel) => {
+              const formattedPlayLevel = playLevel.charAt(0).toUpperCase() + playLevel.slice(1)
+              return (
+                <Option key={playLevel} value={playLevel}>
+                  {formattedPlayLevel}
+                </Option>
+              )
+            })}
+          </Select>
+          <Button colorScheme="primary" size="lg" variant="gradient">
+            {submitLabel}
+          </Button>
+        </Stack>
+      </VStack>
+    )
+  },
+)
 
 QuickBook.displayName = "QuickBook"
