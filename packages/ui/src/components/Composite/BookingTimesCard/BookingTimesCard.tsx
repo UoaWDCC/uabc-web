3a1b7935bc4588b6f929132ae8cad376410ce95b
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-07-06 13:48:37 +1200

    perf(BookingTimesCard): memoize bookingtimescard

--- packages/ui/src/components/Composite/BookingTimesCard/BookingTimesCard.tsx
+++ packages/ui/src/components/Composite/BookingTimesCard/BookingTimesCard.tsx
@@ -10,7 +10,7 @@ import {
   Spacer,
   Text,
 } from "@yamada-ui/react"
-import { useMemo } from "react"
+import { memo, useMemo } from "react"
 import { Button, Heading } from "../../Primitive"
 import { separatorAfterStyles, styles } from "./BookingTimesCard.style"
 
@@ -44,7 +44,7 @@ export interface BookingTimesCardProps {
  * The BookingTimesCard component displays a card with a title, booking time, and location.
  *
  * @param props Props for the BookingTimesCard component.
- * @returns A BookingTimesCard component.
+ * @returns A memoized BookingTimesCard component.
  *
  * @example
  * // Default usage
@@ -72,66 +72,65 @@ export interface BookingTimesCardProps {
  *   type="full"
  * />
  */
-export const BookingTimesCard = ({
-  title,
-  bookingTime,
-  location,
-  type = "default",
-}: BookingTimesCardProps) => {
-  const cardStyles: CardProps = useMemo(
-    () => ({
-      ...(styles[type as keyof typeof styles] ?? {}),
-    }),
-    [type],
-  )
-  const isDisabled = type === "full"
+export const BookingTimesCard = memo(
+  ({ title, bookingTime, location, type = "default" }: BookingTimesCardProps) => {
+    const cardStyles: CardProps = useMemo(
+      () => ({
+        ...(styles[type as keyof typeof styles] ?? {}),
+      }),
+      [type],
+    )
+    const isDisabled = type === "full"
 
-  return (
-    <Card
-      alignItems="center"
-      dropShadow="drop-shadow(0px 0px 20px rgba(105, 105, 105, 0.50))"
-      gap="md"
-      justifyContent="center"
-      minWidth="md"
-      p="lg"
-      position="relative"
-      rounded="2xl"
-      {...cardStyles}
-    >
-      <CardHeader p="0">
-        <Heading.h3 fontSize="xl" fontWeight="semibold" textAlign="left">
-          {title}
-        </Heading.h3>
-        <Spacer />
-        <HStack gap="sm">
-          <Clock10Icon />
-          <Text>{bookingTime}</Text>
-        </HStack>
-      </CardHeader>
-      <Separator _after={separatorAfterStyles} position="relative" />
-      <CardBody alignSelf="flex-start" p="0" width="full">
-        <HStack>
-          <MapPinIcon height="24px" width="24px" />
-          <Text fontSize="sm">{location}</Text>
-        </HStack>
-        <Separator _after={separatorAfterStyles} borderColor="transparent" position="relative" />
-      </CardBody>
-      <Float placement="end-center">
-        <Button
-          bgGradient={
-            type === "selected"
-              ? "radial-gradient(1120.78% 150.46% at 123.73% 123.3%, #282828 0%, #27164E 100%)"
-              : undefined
-          }
-          colorScheme="primary"
-          disabled={isDisabled}
-          fontWeight="normal"
-          size="sm"
-          variant={type === "selected" ? "gradient" : "solid"}
-        >
-          {type === "selected" ? "Unselect" : type === "full" ? "Full" : "Select"}
-        </Button>
-      </Float>
-    </Card>
-  )
-}
+    return (
+      <Card
+        alignItems="center"
+        dropShadow="drop-shadow(0px 0px 20px rgba(105, 105, 105, 0.50))"
+        gap="md"
+        justifyContent="center"
+        minWidth="md"
+        p="lg"
+        position="relative"
+        rounded="2xl"
+        {...cardStyles}
+      >
+        <CardHeader p="0">
+          <Heading.h3 fontSize="xl" fontWeight="semibold" textAlign="left">
+            {title}
+          </Heading.h3>
+          <Spacer />
+          <HStack gap="sm">
+            <Clock10Icon />
+            <Text>{bookingTime}</Text>
+          </HStack>
+        </CardHeader>
+        <Separator _after={separatorAfterStyles} position="relative" />
+        <CardBody alignSelf="flex-start" p="0" width="full">
+          <HStack>
+            <MapPinIcon height="24px" width="24px" />
+            <Text fontSize="sm">{location}</Text>
+          </HStack>
+          <Separator _after={separatorAfterStyles} borderColor="transparent" position="relative" />
+        </CardBody>
+        <Float placement="end-center">
+          <Button
+            bgGradient={
+              type === "selected"
+                ? "radial-gradient(1120.78% 150.46% at 123.73% 123.3%, #282828 0%, #27164E 100%)"
+                : undefined
+            }
+            colorScheme="primary"
+            disabled={isDisabled}
+            fontWeight="normal"
+            size="sm"
+            variant={type === "selected" ? "gradient" : "solid"}
+          >
+            {type === "selected" ? "Unselect" : type === "full" ? "Full" : "Select"}
+          </Button>
+        </Float>
+      </Card>
+    )
+  },
+)
+
+BookingTimesCard.displayName = "BookingTimesCard"
