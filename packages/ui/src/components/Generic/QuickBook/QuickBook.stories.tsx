b5c12c9b8bf4af9c591f0f25b9a4f05c1e6ead96
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-07 15:23:59 +1200

    refactor: use SelectItem[] to pass in options for the Select components

--- packages/ui/src/components/Generic/QuickBook/QuickBook.stories.tsx
+++ packages/ui/src/components/Generic/QuickBook/QuickBook.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/nextjs"
+import type { SelectItem } from "@yamada-ui/react"
 import { QuickBook } from "./QuickBook"
 
 type Story = StoryObj<typeof QuickBook>
@@ -14,16 +15,23 @@ const meta = {
 
 export default meta
 
-const sampleLocationsAndTimes: { id: string; label: string }[] = [
-  { id: "lr66j8dobrqoodojr460p9jx", label: "Tue | 7:30pm - 10:00pm | UOA Recreation Centre" },
-  { id: "prfym4c0sz7dcj4v8gmkvg2u", label: "Wed | 5:00pm - 7:00pm | ABA" },
-  { id: "34fds8276v9pqgv2w6zpmy4a", label: "Thu | 7:30pm - 10:00pm | King's School" },
-  { id: "smpt3sa8hgwn19osvdf3l3wa", label: "Fri | 7:30pm - 10:00pm | UOA Recreation Centre" },
-  { id: "o2akj1d225h1fecaydjxze1b", label: "Sat | 4:00pm - 6:00pm | ABA" },
+const sampleLocationsAndTimes: SelectItem[] = [
+  { label: "Tue | 7:30pm - 10:00pm | UOA Recreation Centre", value: "lr66j8dobrqoodojr460p9jx" },
+  { label: "Wed | 5:00pm - 7:00pm | ABA", value: "prfym4c0sz7dcj4v8gmkvg2u" },
+  { label: "Thu | 7:30pm - 10:00pm | King's School", value: "34fds8276v9pqgv2w6zpmy4a" },
+  { label: "Fri | 7:30pm - 10:00pm | UOA Recreation Centre", value: "smpt3sa8hgwn19osvdf3l3wa" },
+  { label: "Sat | 4:00pm - 6:00pm | ABA", value: "o2akj1d225h1fecaydjxze1b" },
+]
+
+const samplePlayLevels: SelectItem[] = [
+  { label: "beginner", value: "beginner" },
+  { label: "intermediate", value: "intermediate" },
+  { label: "advanced", value: "advanced" },
 ]
 
 export const Default: Story = {
   args: {
-    schedules: sampleLocationsAndTimes,
+    locationAndTimeOptions: sampleLocationsAndTimes,
+    skillLevelOptions: samplePlayLevels,
   },
 }
--- packages/ui/src/components/Generic/QuickBook/QuickBook.tsx
+++ packages/ui/src/components/Generic/QuickBook/QuickBook.tsx
@@ -1,37 +1,37 @@
-import { PlayLevel } from "@repo/shared"
 import { Button, Heading, Select } from "@repo/ui/components/Primitive"
 import { CalendarClockIcon, CircleGaugeIcon } from "@yamada-ui/lucide"
-import { memo, Option, Stack, VStack } from "@yamada-ui/react"
+import { memo, type SelectItem, Stack, VStack } from "@yamada-ui/react"
 
 export interface QuickBookProps {
   title?: string
   submitLabel?: string
-  schedules: { id: string; label: string }[]
+  locationAndTimeOptions: SelectItem[]
+  skillLevelOptions: SelectItem[]
 }
 
 export const QuickBook = memo(
-  ({ title = "Quick Book", submitLabel = "Book Now", schedules }: QuickBookProps) => {
+  ({
+    title = "Quick Book",
+    submitLabel = "Book Now",
+    locationAndTimeOptions,
+    skillLevelOptions,
+  }: QuickBookProps) => {
     return (
       <VStack bgColor="secondary.800" borderRadius="3xl" borderWidth="medium" p="md">
         <Heading.h2>{title}</Heading.h2>
         <Stack direction={{ base: "column", md: "row" }}>
-          <Select icon={<CalendarClockIcon fontSize={24} />} label="Location & Time" stylised>
-            {schedules.map((schedule) => (
-              <Option key={schedule.id} value={schedule.label}>
-                {schedule.label}
-              </Option>
-            ))}
-          </Select>
-          <Select icon={<CircleGaugeIcon fontSize={24} />} label="Skill Level" stylised>
-            {Object.keys(PlayLevel).map((playLevel) => {
-              const formattedPlayLevel = playLevel.charAt(0).toUpperCase() + playLevel.slice(1)
-              return (
-                <Option key={playLevel} value={playLevel}>
-                  {formattedPlayLevel}
-                </Option>
-              )
-            })}
-          </Select>
+          <Select
+            icon={<CalendarClockIcon fontSize={24} />}
+            items={locationAndTimeOptions}
+            label="Location & Time"
+            stylised
+          />
+          <Select
+            icon={<CircleGaugeIcon fontSize={24} />}
+            items={skillLevelOptions}
+            label="Skill Level"
+            stylised
+          />
           <Button colorScheme="primary" size="lg" variant="gradient">
             {submitLabel}
           </Button>
