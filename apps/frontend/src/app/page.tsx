aa3edef605582ecad59d077b74981895fc0de571
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-08 23:41:38 +1200

    feat(homepage): add initial structure for homepage and discuss FAQ handling

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -2,7 +2,9 @@ import { FAQ } from "@repo/ui/components/Generic"
 import { Container } from "@yamada-ui/react"
 import { getFaq } from "@/lib/api/endpoints"
 
+// TODO: Implement actual homepage
 export default async function Home() {
+  //  TODO: Discuss with team if we want to fail the build if faq is not found
   const { data: faq } = await getFaq()
 
   return (
