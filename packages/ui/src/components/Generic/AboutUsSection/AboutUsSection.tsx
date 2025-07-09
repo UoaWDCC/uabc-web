7ebd8d464ffec449aed1596dbffa92902e917bc9
mikais13 <mikai.somerville@gmail.com>
2025-07-09 22:16:45 +1200

    feat(about-us): add AboutUsSection component

---
+++ packages/ui/src/components/Generic/AboutUsSection/AboutUsSection.tsx
@@ -0,0 +1,43 @@
+import { AboutUsCard, type AboutUsCardProps } from "@repo/ui/components/Generic/AboutUsCard"
+import {
+  AboutUsCarousel,
+  type AboutUsCarouselProps,
+} from "@repo/ui/components/Generic/AboutUsCarousel"
+import { Button, Heading } from "@repo/ui/components/Primitive"
+import { Container, Flex, VStack } from "@yamada-ui/react"
+import Link from "next/link"
+
+export interface AboutUsSectionProps extends AboutUsCarouselProps {
+  cards: AboutUsCardProps[]
+}
+
+export const AboutUsSection = ({ cards, ...carouselProps }: AboutUsSectionProps) => {
+  return (
+    <Container as={VStack} centerContent gap="md" padding="calc(lg - sm)">
+      <Heading.h2 fontWeight="semibold" w="full">
+        About Us
+      </Heading.h2>
+      <VStack gap="2xl">
+        <AboutUsCarousel
+          data-testid="about-us-carousel"
+          wrapperProps={{ padding: { sm: "sm" } }}
+          {...carouselProps}
+          autoplay
+          delay={4000}
+          height="auto"
+          width="full"
+        />
+        <Flex flexDir={{ base: "column", md: "row" }} gap="md">
+          {cards.map((card) => (
+            <AboutUsCard key={card.title} {...card} />
+          ))}
+        </Flex>
+      </VStack>
+      <Button as={Link} colorScheme="primary" href="/about" size="lg">
+        Learn More
+      </Button>
+    </Container>
+  )
+}
+
+AboutUsSection.displayName = "AboutUsSection"
