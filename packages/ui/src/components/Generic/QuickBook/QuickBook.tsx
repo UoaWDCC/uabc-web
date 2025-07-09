596528484119f8690643e09b643e5547bcbf53b8
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-09 16:31:55 +1200

    refactor: use PlayLevel enum for skillLevelOptions

--- packages/ui/src/components/Generic/QuickBook/QuickBook.tsx
+++ packages/ui/src/components/Generic/QuickBook/QuickBook.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod"
+import { PlayLevel } from "@repo/shared"
 import { Button, Heading, Select } from "@repo/ui/components/Primitive"
 import { CalendarClockIcon, CircleGaugeIcon } from "@yamada-ui/lucide"
 import { Grid, GridItem, memo, noop, type SelectItem, VStack } from "@yamada-ui/react"
@@ -12,16 +13,20 @@ export interface QuickBookProps {
   title?: string
   submitLabel?: string
   locationAndTimeOptions: SelectItem[]
-  skillLevelOptions: SelectItem[]
   onSubmit?: SubmitHandler<UIQuickBookFormValues>
 }
+
+const skillLevelOptions = Object.values(PlayLevel).map((playLevel) => ({
+  value: playLevel,
+  label: playLevel.charAt(0).toUpperCase() + playLevel.slice(1),
+  // TODO: Change PlayLevel enum values to title case and remove string manipulationg here
+}))
 
 export const QuickBook = memo(
   ({
     title = "Quick Book",
     submitLabel = "Book Now",
     locationAndTimeOptions,
-    skillLevelOptions,
     onSubmit,
   }: QuickBookProps) => {
     const {
