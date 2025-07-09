69f39d5146b16fa88ef0a125575d0548da12aa8d
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-07-09 20:08:59 +1200

    refactor: remove unused style file

--- packages/ui/src/components/Composite/BookingTimesCardGroup/BookingTimesCard.stories.tsx
+++ packages/ui/src/components/Composite/BookingTimesCardGroup/BookingTimesCard.stories.tsx
@@ -5,90 +5,46 @@ import { BookingTimesCardGroup } from "./BookingTimesCardGroup"
 const meta: Meta<typeof BookingTimesCardGroup> = {
   title: "Composite Components / BookingTimesCardGroup",
   component: BookingTimesCardGroup,
-  argTypes: {
-    // label: {
-    //   control: "text",
-    //   description: "The component's heading title label",
-    //   table: {
-    //     type: { summary: "string" },
-    //     defaultValue: { summary: "Tuesday, 12th May" },
-    //   },
-    // },
-    // value: {
-    //   control: "text",
-    //   description: "The component's value",
-    //   table: {
-    //     type: { summary: "string" },
-    //     defaultValue: { summary: "e7f2ge97g2fu9beu2ge97gfu9i2heb" },
-    //   },
-    // },
-    // location: {
-    //   control: "text",
-    //   description: "The component's location",
-    //   table: {
-    //     type: { summary: "string" },
-    //     defaultValue: { summary: "UoA Hiwa Center" },
-    //   },
-    // },
-    // bookingTime: {
-    //   control: "text",
-    //   description: "The component's booking time",
-    //   table: {
-    //     type: { summary: "string" },
-    //     defaultValue: { summary: "7:30 - 10pm" },
-    //   },
-    // },
-    // full: {
-    //   control: "boolean",
-    //   description: "Whether the component is full",
-    //   table: {
-    //     type: { summary: "boolean" },
-    //     defaultValue: { summary: "false" },
-    //   },
-    // },
-  },
 }
 
 export default meta
 
 type Story = StoryFn<typeof BookingTimesCardGroup>
 
+const defaultData = [
+  {
+    location: "UoA Hiwa Center",
+    bookingTime: "19:30 - 20:00",
+    label: "Tuesday, 12th May",
+    value: "1",
+  },
+  {
+    location: "UoA Hiwa Center",
+    bookingTime: "19:30 - 20:00",
+    label: "Tuesday, 12th May",
+    value: "2",
+  },
+  {
+    location: "UoA Hiwa Center",
+    bookingTime: "19:30 - 20:00",
+    label: "Tuesday, 12th May",
+    value: "3",
+  },
+]
+
 export const TypesAndStates: Story = (_args) => {
   const states = ["normal", "disabled", "error"]
-
-  const data = [
-    {
-      location: "UoA Hiwa Center",
-      bookingTime: "19:30 - 20:00",
-      label: "Tuesday, 12th May",
-      value: "1",
-    },
-    {
-      location: "UoA Hiwa Center",
-      bookingTime: "19:30 - 20:00",
-      label: "Tuesday, 12th May",
-      value: "2",
-    },
-    {
-      location: "UoA Hiwa Center",
-      bookingTime: "19:30 - 20:00",
-      label: "Tuesday, 12th May",
-      value: "3",
-    },
-  ]
 
   return (
     <PropsTable rows={states} variant="column">
       {(_col, row, _index) => {
-        // Determine state
         const isDisabled = row === "disabled"
         const isError = row === "error"
 
-        // Map data to BookingTimesCardProps
-        const items = data.map((item) => ({
+        const items = defaultData.map((item) => ({
           ...item,
           invalid: isError,
-          disabled: isDisabled,
+          full: isDisabled,
         }))
 
         return <BookingTimesCardGroup defaultValue={["1"]} items={items} />
