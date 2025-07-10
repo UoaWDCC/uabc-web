21d2c8e5fb05289552f6fdc9fce36bdc6ff6dd1a
Derrick Trang <derrick.trang100@hotmail.com>
2025-07-11 00:34:26 +1200

    fix(userRequestSchema): fixed schema for types imports, nativeEnum for enums zod

--- packages/shared/src/schemas/user.ts
+++ packages/shared/src/schemas/user.ts
@@ -1,24 +1,25 @@
 import { z } from "zod"
-import type { CreateUserData, EditUserData } from "@/types"
+import {
+  type CreateUserData,
+  type EditUserData,
+  Gender,
+  MembershipType,
+  PlayLevel,
+  University,
+} from "../types"
 
 export const CreateUserRequestSchema = z.object({
   firstName: z.string().min(1).max(30),
   lastName: z.string().nullable().optional(),
   email: z.string().email(),
-  role: z.enum(["member", "casual", "admin"]),
+  role: z.nativeEnum(MembershipType),
   phoneNumber: z.string().nullable().optional(),
-  playLevel: z.enum(["beginner", "intermediate", "advanced"]).nullable().optional(),
-  gender: z
-    .enum(["male", "female", "non-binary", "other", "prefer-not-to-answer"])
-    .nullable()
-    .optional(),
+  playLevel: z.nativeEnum(PlayLevel).nullable().optional(),
+  gender: z.nativeEnum(Gender).nullable().optional(),
   dietaryRequirements: z.string().nullable().optional(),
   studentId: z.string().nullable().optional(),
   studentUpi: z.string().nullable().optional(),
-  university: z
-    .enum(["UoA", "AUT", "Massey University", "Other", "Working", "Not a student"])
-    .nullable()
-    .optional(),
+  university: z.nativeEnum(University).nullable().optional(),
   remainingSessions: z.number().nullable().optional(),
   image: z.string().nullable().optional(),
 }) satisfies z.ZodType<CreateUserData>
