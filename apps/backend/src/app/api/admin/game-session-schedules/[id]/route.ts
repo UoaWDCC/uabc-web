bef6b04a64a347b7ee8af18813242fa453b3a417
xtncz <ascendus.dev@gmail.com>
2025-07-11 16:59:32 +1200

    feat: add delete gamesessionschedules route

---
+++ apps/backend/src/app/api/admin/game-session-schedules/[id]/route.ts
@@ -0,0 +1,38 @@
+import { getReasonPhrase, StatusCodes } from "http-status-codes"
+import { type NextRequest, NextResponse } from "next/server"
+import { NotFound } from "payload"
+import { Security } from "@/business-layer/middleware/Security"
+import GameSessionDataService from "@/data-layer/services/GameSessionDataService"
+
+class GameSessionScheduleRouteWrapper {
+  /**
+   * DELETE method to delete a game session schedule.
+   *
+   * @param _req The request object
+   * @param params route parameters containing the GameSessionSchedule ID
+   * @returns No content status code
+   */
+  @Security("jwt", ["admin"])
+  static async DELETE(_req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+      const { id } = await params
+      const gameSessionDataService = new GameSessionDataService()
+      await gameSessionDataService.deleteGameSessionSchedule(id)
+      return new NextResponse(null, { status: StatusCodes.NO_CONTENT })
+    } catch (error) {
+      if (error instanceof NotFound) {
+        return NextResponse.json(
+          { error: "Game Session Scgedyke not found" },
+          { status: StatusCodes.NOT_FOUND },
+        )
+      }
+      console.error(error)
+      return NextResponse.json(
+        { error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR) },
+        { status: StatusCodes.INTERNAL_SERVER_ERROR },
+      )
+    }
+  }
+}
+
+export const DELETE = GameSessionScheduleRouteWrapper.DELETE
