f834b0f1e796551736cbaf7b44ae1ccbb2991c67
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-11 15:51:59 +1200

    feat: add POST endpoint for GameSessionSchedules (and zod schemas)

---
+++ packages/shared/src/schemas/game-session.ts
@@ -0,0 +1,25 @@
+import z from "zod"
+import type { GameSessionSchedule } from "@/payload-types"
+import { type CreateGameSessionScheduleData, Weekday } from "../types"
+import { SemesterSchema } from "./semester"
+
+export const GameSessionScheduleSchema = z.object({
+  id: z.string(),
+  semester: z.union([z.string(), SemesterSchema]),
+  day: z.nativeEnum(Weekday),
+  startTime: z.string().datetime({ message: "" }),
+  endTime: z.string().datetime({ message: "" }),
+  capacity: z.number(),
+  casualCapacity: z.number(),
+  updatedAt: z.string(),
+  createdAt: z.string(),
+}) satisfies z.ZodType<GameSessionSchedule>
+
+export const CreateGameSessionScheduleRequestSchema = z.object({
+  semester: z.union([z.string(), SemesterSchema]),
+  day: z.nativeEnum(Weekday),
+  startTime: z.string().datetime({ message: "Invalid date format, should be in ISO 8601 format" }),
+  endTime: z.string().datetime({ message: "Invalid date format, should be in ISO 8601 format" }),
+  capacity: z.number(),
+  casualCapacity: z.number(),
+}) satisfies z.ZodType<CreateGameSessionScheduleData>
