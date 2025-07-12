f9faded2008401a3f6513cdee617c9ed0968880b
mikais13 <mikai.somerville@gmail.com>
2025-07-13 01:05:07 +1200

    refactor(services): use enum for query keys

--- apps/frontend/src/services/cms/faq/FaqQuery.ts
+++ apps/frontend/src/services/cms/faq/FaqQuery.ts
@@ -1,12 +1,8 @@
 "use client"
 import type { Faq } from "@repo/shared/payload-types"
 import { useQuery } from "@tanstack/react-query"
+import { QueryKeys } from "@/services"
 import { getFaq } from "./FaqService"
-
-/**
- * Key for the FAQ query
- */
-export const FAQ_QUERY_KEY = ["faqs"]
 
 /**
  * Custom hook to fetch {@link Faq} data and cache result using Tanstack Query
@@ -15,7 +11,7 @@ export const FAQ_QUERY_KEY = ["faqs"]
  */
 export function useFaq() {
   return useQuery({
-    queryKey: FAQ_QUERY_KEY,
+    queryKey: [QueryKeys.FAQ_QUERY_KEY],
     queryFn: async () => {
       const { data } = await getFaq()
       return data?.data ?? null
