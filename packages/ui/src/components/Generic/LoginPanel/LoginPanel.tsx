7750fba91218126ff050157e512b4e78d17bf0e7
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-13 13:38:48 +1200

    feat: add styling, and add responsive design so mobile version isn't constrained in width

--- packages/ui/src/components/Generic/LoginPanel/LoginPanel.tsx
+++ packages/ui/src/components/Generic/LoginPanel/LoginPanel.tsx
@@ -49,19 +49,21 @@ export const LoginPanel = memo(({ onSubmit }: LoginPanelProps) => {
     <VStack
       as="form"
       bgColor="secondary.900"
-      borderRadius="3xl"
-      layerStyle="gradientBorder"
+      borderRadius={{ base: undefined, md: "3xl" }}
+      layerStyle={{ base: undefined, md: "gradientBorder" }}
       onSubmit={handleSubmit(onSubmit ?? noop)}
       p="md"
-      w="md"
+      w={{ base: "full", md: "md" }}
     >
-      <UabcLogo />
-      <Heading.h2 display={{ base: "none", md: "block" }} textAlign="center">
-        Welcome back
-      </Heading.h2>
-      <Text display={{ base: "none", md: "block" }} textAlign="center">
-        Please enter your details to sign in
-      </Text>
+      <Center>
+        <UabcLogo />
+      </Center>
+
+      <Center display={{ base: "none", md: "block" }} textAlign="center">
+        <Heading.h2>Welcome back</Heading.h2>
+        <Text>Please enter your details to sign in</Text>
+      </Center>
+
       <TextInput
         data-testid="email"
         errorMessage={errors.email?.message}
@@ -81,25 +83,27 @@ export const LoginPanel = memo(({ onSubmit }: LoginPanelProps) => {
         type={InputType.Password}
       />
       <HStack>
-        <Checkbox label="Remember me" />
+        <Checkbox label="Remember me" textAlign="start" />
         <Spacer />
-        <UILink as={Link} href="/auth/forgot-password" textColor="white">
+        <UILink as={Link} href="/auth/forgot-password" textAlign="end" textColor="white">
           Forgot Password?
         </UILink>
       </HStack>
       <Button colorScheme="primary" loading={isSubmitting} type="submit">
         Sign In
       </Button>
+
       <BreakLine
         label="or"
         separatorProps1={{ border: 0, layerStyle: "fadeLeft" }}
         separatorProps2={{ border: 0, layerStyle: "fadeRight" }}
       />
+
       <span>Google and Apple icons go here</span>
-      <Center>
-        <Text color="gray.100" justifyItems="center">
+      <Center textAlign="center">
+        <Text color="gray.100">
           Don't have an account?&nbsp;
-          <UILink as={Link} color="gray.100" fontWeight="bold" href="/auth/login?open=true">
+          <UILink as={Link} color="gray.100" fontWeight="bold" href="/auth/login">
             Create Account
           </UILink>
         </Text>
