c4f18b7255d6c686a692ca03cdaef50c8f97873b
mikais13 <mikai.somerville@gmail.com>
2025-07-13 15:04:41 +1200

    feat(faq): improve skeleton component's similiarity to faq component

--- packages/ui/src/components/Generic/FAQ/FAQSkeleton.tsx
+++ packages/ui/src/components/Generic/FAQ/FAQSkeleton.tsx
@@ -1,5 +1,15 @@
 import type { FAQ } from "@repo/ui/components/Generic"
-import { Skeleton, VStack } from "@yamada-ui/react"
+import {
+  Accordion,
+  AccordionItem,
+  AccordionLabel,
+  For,
+  IconButton,
+  Skeleton,
+  Text,
+  VStack,
+} from "@yamada-ui/react"
+import { Heading } from "../../Primitive"
 
 /**
  * A skeleton for the {@link FAQ} section, providing a loading state with placeholders for the title and three items.
@@ -19,10 +29,37 @@ export const FAQSkeleton = () => {
       rounded="2xl"
       w="full"
     >
-      <Skeleton data-testid="faq-loading-title" h="36px" w="128px" />
-      <Skeleton data-testid="faq-loading-item" h="75px" />
-      <Skeleton data-testid="faq-loading-item" h="75px" />
-      <Skeleton data-testid="faq-loading-item" h="75px" />
+      <Skeleton data-testid="faq-loading-title">
+        <Heading as="h2" fontSize="4xl">
+          FAQ
+        </Heading>
+      </Skeleton>
+      <Accordion
+        icon={
+          <Skeleton borderRadius="md">
+            <IconButton />
+          </Skeleton>
+        }
+        pointerEvents="none"
+        w="full"
+      >
+        <For each={Array.from({ length: 3 })}>
+          {(_, index) => (
+            <AccordionItem
+              _last={{
+                borderBottomWidth: "0",
+              }}
+              key="faq-skeleton-item"
+            >
+              <AccordionLabel fontSize="lg" p={{ base: "md", lg: "calc(lg - sm)" }}>
+                <Skeleton data-testid="faq-loading-item">
+                  <Text textOverflow="clip">Question {index + 1} Placeholder Text</Text>
+                </Skeleton>
+              </AccordionLabel>
+            </AccordionItem>
+          )}
+        </For>
+      </Accordion>
     </VStack>
   )
 }
