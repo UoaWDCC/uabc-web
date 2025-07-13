f9725c0551edb4345c90c8bf4b7936e266ca4f97
mikais13 <mikai.somerville@gmail.com>
2025-07-13 19:57:30 +1200

    fix(navbar): use correct function to prefetch data

--- apps/frontend/src/components/server/NavigationBarServerSection.tsx
+++ apps/frontend/src/components/server/NavigationBarServerSection.tsx
@@ -2,14 +2,14 @@ import { getQueryClient } from "@repo/ui/components/Provider/getQueryClient"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 import NavigationBarSection from "@/components/client/NavigationBarSection"
 import { QueryKeys } from "@/services"
-import { useNavigationBar } from "@/services/cms/navbar/NavigationBarQuery"
+import { getNavigationBar } from "@/services/cms/navbar/NavigationBarService"
 
 export const NavigationBarServerSection = () => {
   const queryClient = getQueryClient()
 
   queryClient.prefetchQuery({
     queryKey: [QueryKeys.NAVIGATION_BAR_QUERY_KEY],
-    queryFn: useNavigationBar,
+    queryFn: getNavigationBar,
   })
 
   return (
