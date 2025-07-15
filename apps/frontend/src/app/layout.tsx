32622c8af51331d0a15ec68e698c4b0aa456e97c
xtncz <ascendus.dev@gmail.com>
2025-07-15 23:45:22 +1200

    refactor: create constants for title and desc

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -15,14 +15,16 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
 })
 
+const title = "UABC | Home"
+const description =
+  "Welcome to the homepage of UABC, New Zealand's largest student badminton club. We run weekly badminton sessions and fun social events to connect like-minded badminton people! Join our sessions or check our Instagram page for events!"
+
 export const metadata: Metadata = {
-  title: "UABC | Home",
-  description:
-    "Welcome to the homepage of UABC, New Zealand's largest student badminton club. We run weekly badminton sessions and fun social events to connect like-minded badminton people! Join our sessions or check our Instagram page for events!",
+  title,
+  description,
   openGraph: {
-    title: "UABC | Home",
-    description:
-      "Welcome to the homepage of UABC, New Zealand's largest student badminton club. We run weekly badminton sessions and fun social events to connect like-minded badminton people! Join our sessions or check our Instagram page for events!",
+    title,
+    description,
     url: "https://uabc.wddc.co.nz",
     siteName: "UABC",
     images: [
