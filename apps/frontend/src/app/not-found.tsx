82a1a04af086c192c7fb84e56e9561f008abbd89
mikais13 <mikai.somerville@gmail.com>
2025-07-15 14:38:08 +1200

    feat(frontend): add not-found page

---
+++ apps/frontend/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import { NotFoundCard } from "@repo/ui/components/Generic/NotFoundCard"
+
+/**
+ * A 404 Not Found page component for when a page is not found at the specified route.
+ *
+ * @returns A 404 Not Found page with a message and a button to return home.
+ */
+export default function NotFound() {
+  return <NotFoundCard />
+}
