b217d63d70c3015ec7d3d15eef774c6cd054e426
Mikai Somerville <91355759+mikais13@users.noreply.github.com>
2025-07-15 19:44:09 +1200

    feat(navbar): add services for `Navbar` (#478)
    * refactor(navbar): correct navbar global config use of links
    
    * refactor(navbar): use refactored navbar data as props for NavigationBar component
    
    * refactor(faq): move FaqSection component to client component folder
    
    * feat(navbar, services): add navbar service and query
    
    * feat(navbar): add NavigationBarServerSection component
    
    * fix(shared): use correct import path for navbar type in schema
    
    * fix(navbar): use correct function to prefetch data
    
    * fix(navbar): use new navbar global structure in default value
    
    * revert(navbar, globals): revert navbar global structure and use LinkArray interface
    
    * docs(navbar): use correct comments for query and service
    
    * docs(navbar): add comments to navbar section components
    
    * chore(layout): remove unnecessary comments
    
    * revert(config): remove accidental remote pattern
    
    * chore(navbar, services): remove unnecessary vitest imports
    
    * fix(navbar): await prefetching of data
    
    * chore(test-config): exclude frontend services from test coverage
    
    * chore(components): remove duplicate FAQSection files
    
    * feat(navbar): add error boundary to server section for errors thrown during query
    
    * chore(navbar): remove service test

---
+++ apps/frontend/src/components/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React from "react"
+
+/**
+ * Props for the ErrorBoundary component.
+ */
+interface ErrorBoundaryProps {
+  children: React.ReactNode
+  fallback: React.ReactNode
+}
+
+/**
+ * Interface for the state of the ErrorBoundary component.
+ */
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+/**
+ * ErrorBoundary component to catch JavaScript errors in its child component tree,
+ *
+ * @param children The child components to render when an error does not occur.
+ * @param fallback The fallback UI to render when an error occurs.
+ */
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback
+    }
+
+    return this.props.children
+  }
+}
