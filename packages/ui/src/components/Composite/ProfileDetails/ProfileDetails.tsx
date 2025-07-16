ad88b28ffad7aa1f03f1154ab9d14ab316d82299
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-16 17:44:42 +1200

    docs(profile-details): add jsdoc to ProfileDetails component

--- packages/ui/src/components/Composite/ProfileDetails/ProfileDetails.tsx
+++ packages/ui/src/components/Composite/ProfileDetails/ProfileDetails.tsx
@@ -2,8 +2,18 @@ import type { Field, NullableFormData } from "@repo/ui/components/Generic"
 import { UserProfileCard, type UserProfileCardProps } from "@repo/ui/components/Generic"
 import { useMutation } from "@tanstack/react-query"
 
+/**
+ * The props for the ProfileDetails component.
+ * @template T The tuple of fields for the form.
+ */
 interface ProfileDetailsProps<T extends readonly Field[]> extends UserProfileCardProps<T> {}
 
+/**
+ * ProfileDetails renders a user profile details card with asynchronous save handling.
+ *
+ * @template T The tuple of fields for the form.
+ * @param props ProfileDetailsProps
+ */
 export const ProfileDetails = <T extends readonly Field[]>({
   onSave,
   ...props
