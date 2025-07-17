43556e7835587873fd147623c700bd06c646bfba
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-17 22:01:47 +1200

    feat(booking-confirmed-popup): add a booking-confirmed composite component

---
+++ packages/ui/src/components/Composite/BookingConfirmedPopup/BookingConfirmedPopup.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { MembershipType } from "@repo/shared"
+import { Button, Heading } from "@repo/ui/components/Primitive"
+import { usePopupState } from "@repo/ui/hooks"
+import { StickerIcon } from "@yamada-ui/lucide"
+import { Modal, ModalBody, ModalFooter, ModalHeader, type ModalProps, Text } from "@yamada-ui/react"
+import Link from "next/link"
+import type { FC } from "react"
+
+/**
+ * Props for the BookingConfirmedPopup component.
+ */
+interface BookingConfirmedPopupProps extends ModalProps {
+  /**
+   * The value of the popup.
+   */
+  title: string
+  /**
+   * The message of the popup.
+   */
+  message: string
+  /**
+   * The additional message of the popup.
+   */
+  additionalMessage: string
+}
+
+/**
+ * A modal that displays a booking confirmation message.
+ *
+ * @param props The props for the modal.
+ * @returns The modal component.
+ *
+ * @example
+ * // Basic usage (the popup will open if the search param is set)
+ * // To open the popup, set the search param: ?booking-confirmed-popup=open
+ * import { BookingConfirmedPopup } from "@repo/ui/components/Composite"
+ *
+ * <BookingConfirmedPopup />
+ */
+export const BookingConfirmedPopup: FC<BookingConfirmedPopupProps> = ({
+  title,
+  message,
+  additionalMessage,
+  ...props
+}) => {
+  const { value, close, isOpen } = usePopupState({
+    popupId: "booking-confirmed-popup",
+    initialValue: MembershipType.member,
+  })
+
+  return (
+    <Modal onClose={close} open={isOpen} p="lg" rounded="3xl" size="xl" {...props}>
+      <ModalHeader alignItems="center" flexDirection="column">
+        <StickerIcon boxSize="2xs" />
+        <Heading.h2 fontSize="3xl">{title}</Heading.h2>
+      </ModalHeader>
+      <ModalBody textAlign="center">
+        <Text>{message}</Text>
+        {value === MembershipType.casual && additionalMessage && <Text>{additionalMessage}</Text>}
+      </ModalBody>
+      <ModalFooter alignItems="center" justifyContent="center">
+        <Button as={Link} colorScheme="primary" href="/booking">
+          View Booking
+        </Button>
+      </ModalFooter>
+    </Modal>
+  )
+}
