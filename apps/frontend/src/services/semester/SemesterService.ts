5bb7c3bcd8f25a90aa83c1418b2de5b93466816a
mikais13 <mikai.somerville@gmail.com>
2025-07-17 18:50:46 +1200

    feat(services): add semester service

---
+++ apps/frontend/src/services/semester/SemesterService.ts
@@ -0,0 +1,36 @@
+import { GetSemesterResponseSchema, GetSemestersResponseSchema } from "@repo/shared/schemas"
+import { StatusCodes } from "http-status-codes"
+import { apiClient } from "@/lib/api/client"
+
+const SemesterService = {
+  /**
+   * Retrieve a specific semester by its ID.
+   *
+   * @param id The ID of the semester to retrieve.
+   * @returns The semester data for the given semester ID.
+   */
+  getSemester: async (id: string) => {
+    const { data: semester, status } = await apiClient.get(
+      `/api/semester/${id}`,
+      GetSemesterResponseSchema,
+    )
+    if (status !== StatusCodes.OK) throw new Error(`Failed to retrieve semester with id: ${id}`)
+    return semester
+  },
+
+  /**
+   * Retrieve all semesters.
+   *
+   * @returns A list of all semesters.
+   */
+  getSemesters: async () => {
+    const { data: semesters, status } = await apiClient.get(
+      "/api/semesters",
+      GetSemestersResponseSchema,
+    )
+    if (status !== StatusCodes.OK) throw new Error("Failed to retrieve semesters")
+    return semesters
+  },
+} as const
+
+export default SemesterService
