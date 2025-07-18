cb1bc5b063876557e4424248a8fd62495f3ae73a
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-19 02:34:52 +1200

    refactor(tos): optimize getTos function by utilizing caching for improved performance

--- apps/frontend/src/services/cms/tos/TosService.ts
+++ apps/frontend/src/services/cms/tos/TosService.ts
@@ -1,8 +1,9 @@
 import { GetTosResponseSchema } from "@repo/shared"
+import { cache } from "react"
 import { apiClient } from "@/lib/api/client"
 import { QueryKeys } from "@/services"
 
-export const getTos = async () => {
+export const getTos = cache(async () => {
   "use server"
   return await apiClient.get("/api/globals/tos", GetTosResponseSchema, [QueryKeys.TOS_QUERY_KEY])
-}
+})
