88a4f4be56d3ef57f6144c27a68365519212b111
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-18 22:56:12 +1200

    feat: wrap QueryClientProvider around <Profile /> in tests

--- apps/frontend/src/app/profile/page.test.tsx
+++ apps/frontend/src/app/profile/page.test.tsx
@@ -1,15 +1,32 @@
 import { bookingsMock, casualUserMock } from "@repo/shared/mocks"
 import { render } from "@repo/ui/test-utils"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Profile from "./page"
 
 // TODO: rewrite tests after proper auth is implemented
 describe("<Profile />", () => {
+  let queryClient: QueryClient
+  let wrapper: ({ children }: { children: React.ReactNode }) => React.JSX.Element
+
+  beforeEach(() => {
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    })
+    wrapper = ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+  })
+
   it("should export the Profile component", () => {
     expect(Profile).toBeDefined()
   })
 
   it("should render the user panel and profile details properly given the user is casualUserMock", async () => {
-    const { getAllByText } = render(await Profile())
+    const { getAllByText } = render(await Profile(), { wrapper: wrapper })
     expect(`${casualUserMock.firstName} ${casualUserMock.lastName}`).toBeInTheDocument()
     expect(casualUserMock.role).toBeInTheDocument()
     expect(casualUserMock.phoneNumber ?? "--").toBeInTheDocument()
@@ -21,13 +38,13 @@ describe("<Profile />", () => {
   })
 
   it("should render the profile booking panel", async () => {
-    const { getAllByTestId } = render(await Profile())
+    const { getAllByTestId } = render(await Profile(), { wrapper: wrapper })
     expect("Your Bookings").toBeInTheDocument()
     expect(getAllByTestId("booking-card")).toHaveLength(bookingsMock.length)
   })
 
   it("should render the additional info properly given the user is casualUserMock", async () => {
-    render(await Profile())
+    render(await Profile(), { wrapper: wrapper })
     expect("Additional Info").toBeInTheDocument()
     expect(casualUserMock.gender).toBeInTheDocument()
     expect(casualUserMock.playLevel).toBeInTheDocument()
