b921a94df30144858a54535d42d3aa91dbc8eeae
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-19 00:40:20 +1200

    refactor(tos): streamline Terms of Service structure and API response

--- apps/backend/src/app/api/globals/tos/route.ts
+++ apps/backend/src/app/api/globals/tos/route.ts
@@ -1,12 +1,10 @@
-import type { GetTosResponse } from "@repo/shared"
-import { StatusCodes } from "http-status-codes"
 import { NextResponse } from "next/server"
 import { payload } from "@/data-layer/adapters/Payload"
 
-export const GET = async (): Promise<NextResponse<GetTosResponse>> => {
+export const GET = async () => {
   const tos = await payload.findGlobal({
     slug: "termsOfService",
   })
 
-  return NextResponse.json({ data: tos }, { status: StatusCodes.OK })
+  return NextResponse.json({ data: tos })
 }
