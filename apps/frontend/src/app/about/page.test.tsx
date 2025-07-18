429053ca800364c01b83a0a61b8769241fc3f196
mikais13 <mikai.somerville@gmail.com>
2025-07-18 17:31:04 +1200

    feat(frontend): add about page with under construction card

---
+++ apps/frontend/src/app/about/page.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@/test-config/test-utils"
+import About from "./page"
+
+describe("<About />", () => {
+  it("should export the Events component", () => {
+    expect(About).toBeDefined()
+    expect(About).toBeTruthy()
+  })
+
+  it("should render the about page with under construction message", () => {
+    render(<About />)
+    expect(screen.getByText("Feature is Under Construction 🔧")).toBeInTheDocument()
+    expect(screen.getByText("Our team is busy working on this page.")).toBeInTheDocument()
+  })
+})
