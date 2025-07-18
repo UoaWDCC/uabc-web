3600ae34c8e18cd033dbbca01f78b0597236e927
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-18 23:12:56 +1200

    fix: amend expect statements in /profile tests

--- apps/frontend/src/app/profile/page.test.tsx
+++ apps/frontend/src/app/profile/page.test.tsx
@@ -26,30 +26,32 @@ describe("<Profile />", () => {
   })
 
   it("should render the user panel and profile details properly given the user is casualUserMock", async () => {
-    const { getAllByText } = render(await Profile(), { wrapper: wrapper })
+    render(await Profile(), { wrapper: wrapper })
     expect(
       screen.getByText(`${casualUserMock.firstName} ${casualUserMock.lastName}`),
     ).toBeInTheDocument()
-    expect(casualUserMock.role).toBeInTheDocument()
-    expect(casualUserMock.phoneNumber ?? "--").toBeInTheDocument()
-    expect(`Sessions left: ${casualUserMock.remainingSessions ?? 0}`).toBeInTheDocument()
+    expect(screen.getByText(casualUserMock.role)).toBeInTheDocument()
+    expect(screen.getByText(casualUserMock.phoneNumber ?? "--")).toBeInTheDocument()
+    expect(
+      screen.getByText(`Sessions left: ${casualUserMock.remainingSessions ?? 0}`),
+    ).toBeInTheDocument()
 
-    expect("Profile Details").toBeInTheDocument()
-    expect(getAllByText(casualUserMock.firstName)).toHaveLength(3) // user panel, first name and email
-    expect(casualUserMock.email).toBeInTheDocument()
+    expect(screen.getByText("Profile Details")).toBeInTheDocument()
+    expect(screen.getAllByText(casualUserMock.firstName)).toHaveLength(3) // found in user panel, first name and email
+    expect(screen.getByText(casualUserMock.email)).toBeInTheDocument()
   })
 
   it("should render the profile booking panel", async () => {
-    const { getAllByTestId } = render(await Profile(), { wrapper: wrapper })
+    render(await Profile(), { wrapper: wrapper })
     expect(screen.getByText("Your Bookings")).toBeInTheDocument()
-    expect(getAllByTestId("booking-card")).toHaveLength(bookingsMock.length)
+    expect(screen.getAllByTestId("booking-card")).toHaveLength(bookingsMock.length)
   })
 
   it("should render the additional info properly given the user is casualUserMock", async () => {
     render(await Profile(), { wrapper: wrapper })
     expect(screen.getByText("Additional Info")).toBeInTheDocument()
-    expect(casualUserMock.gender).toBeInTheDocument()
-    expect(casualUserMock.playLevel).toBeInTheDocument()
-    expect(casualUserMock.dietaryRequirements).toBeInTheDocument()
+    expect(screen.getByText(casualUserMock.gender as string)).toBeInTheDocument()
+    expect(screen.getByText(casualUserMock.playLevel as string)).toBeInTheDocument()
+    expect(screen.getByText(casualUserMock.dietaryRequirements as string)).toBeInTheDocument()
   })
 })
