03c0fed2bdefb810ea79ed6f5c36b1af70d5b485
mikais13 <mikai.somerville@gmail.com>
2025-07-18 17:30:50 +1200

    feat(frontend): add contact page with under construction card

---
+++ apps/frontend/src/app/contact/page.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@/test-config/test-utils"
+import Contact from "./page"
+
+describe("<About />", () => {
+  it("should export the Events component", () => {
+    expect(Contact).toBeDefined()
+    expect(Contact).toBeTruthy()
+  })
+
+  it("should render the contact page with under construction message", () => {
+    render(<Contact />)
+    expect(screen.getByText("Feature is Under Construction 🔧")).toBeInTheDocument()
+    expect(screen.getByText("Our team is busy working on this page.")).toBeInTheDocument()
+  })
+})
