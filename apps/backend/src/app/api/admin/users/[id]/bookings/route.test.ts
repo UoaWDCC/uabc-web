af3f10b4b4c6bc874a0b9815c12a5501822d3526
panmann7 <annlowe789@gmail.com>
2025-07-19 23:31:01 +1200

    feat: add unauthorised access tests

--- apps/backend/src/app/api/admin/users/[id]/bookings/route.test.ts
+++ apps/backend/src/app/api/admin/users/[id]/bookings/route.test.ts
@@ -1,11 +1,11 @@
 import { AUTH_COOKIE_NAME } from "@repo/shared"
-import { MEMBER_USER_UID, memberUserMock } from "@repo/shared/mocks"
+import { CASUAL_USER_UID, MEMBER_USER_UID, memberUserMock } from "@repo/shared/mocks"
 import { getReasonPhrase, StatusCodes } from "http-status-codes"
 import { cookies } from "next/headers"
 import BookingDataService from "@/data-layer/services/BookingDataService"
 import { createMockNextRequest } from "@/test-config/backend-utils"
 import { bookingCreateMock } from "@/test-config/mocks/Booking.mock"
-import { adminToken } from "@/test-config/vitest.setup"
+import { adminToken, casualToken, memberToken } from "@/test-config/vitest.setup"
 import { GET } from "./route"
 
 describe("/api/admin/users/[id]/bookings", async () => {
@@ -13,6 +13,34 @@ describe("/api/admin/users/[id]/bookings", async () => {
   const bookingDataService = new BookingDataService()
 
   describe("GET", () => {
+    it("should return 401 if user is a casual", async () => {
+      cookieStore.set(AUTH_COOKIE_NAME, casualToken)
+
+      const response = await GET(
+        createMockNextRequest(`/api/admin/users/${CASUAL_USER_UID}/bookings`),
+        {
+          params: Promise.resolve({ id: CASUAL_USER_UID }),
+        },
+      )
+      const json = await response.json()
+      expect(response.status).toBe(StatusCodes.UNAUTHORIZED)
+      expect(json.data).toStrictEqual({ error: "No scope" })
+    })
+
+    it("should return 401 if user is a member", async () => {
+      cookieStore.set(AUTH_COOKIE_NAME, memberToken)
+
+      const response = await GET(
+        createMockNextRequest(`/api/admin/users/${MEMBER_USER_UID}/bookings`),
+        {
+          params: Promise.resolve({ id: MEMBER_USER_UID }),
+        },
+      )
+      const json = await response.json()
+      expect(response.status).toBe(StatusCodes.UNAUTHORIZED)
+      expect(json.data).toStrictEqual({ error: "No scope" })
+    })
+
     it("should return all bookings for a specific user", async () => {
       cookieStore.set(AUTH_COOKIE_NAME, adminToken)
 
