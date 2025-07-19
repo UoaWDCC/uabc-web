82f26141345c8fac1fca51305c8a474c3d99d938
mikais13 <mikai.somerville@gmail.com>
2025-07-20 00:50:35 +1200

    feat(services): add mutations for update, delete and create services

--- apps/frontend/src/services/admin/game-session-schedule/AdminGameSessionScheduleQuery.ts
+++ apps/frontend/src/services/admin/game-session-schedule/AdminGameSessionScheduleQuery.ts
@@ -1,9 +1,26 @@
-import type { PaginationQuery } from "@repo/shared"
-import { useInfiniteQuery } from "@tanstack/react-query"
+import type { PaginationQuery, UpdateGameSessionScheduleRequest } from "@repo/shared"
+import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query"
 import { QueryKeys } from "@/services"
 import AdminGameSessionScheduleService from "./AdminGameSessionScheduleService"
 
 const AdminGameSessionScheduleQuery = {
+  /**
+   * Provides a mutation hook for creating a new game session schedule and invalidates the game session schedule query cache.
+   *
+   * @returns A mutation hook that creates a game session schedule.
+   */
+  useCreateGameSessionSchedule: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: AdminGameSessionScheduleService.createGameSessionSchedule,
+      // TODO: when get by id is implemented, do not invalidate get by id queries
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.GAME_SESSION_SCHEDULE_QUERY_KEY],
+        }),
+    })
+  },
+
   /**
    * Retrieves and caches paginated game session schedules.
    *
@@ -20,6 +37,52 @@ const AdminGameSessionScheduleQuery = {
       },
       getNextPageParam: (lastPage) => lastPage?.data?.nextPage ?? undefined,
       getPreviousPageParam: (firstPage) => firstPage?.data?.prevPage ?? undefined,
+    })
+  },
+
+  /**
+   * Provides a mutation hook for updating a game session schedule and invalidates the game session schedule query cache.
+   *
+   * @returns A mutation hook that updates a game session schedule.
+   */
+  useUpdateGameSessionSchedule: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: async ({
+        id,
+        gameSessionScheduleData,
+      }: {
+        id: string
+        gameSessionScheduleData: UpdateGameSessionScheduleRequest
+      }) => {
+        const response = await AdminGameSessionScheduleService.updateGameSessionSchedule(
+          id,
+          gameSessionScheduleData,
+        )
+        return response
+      },
+      // TODO: when get by id is implemented, only invalidate the updated id for get by id, or update the specific query with the updated data
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.GAME_SESSION_SCHEDULE_QUERY_KEY],
+        }),
+    })
+  },
+
+  /**
+   * Provides a mutation hook for deleting a game session schedule and invalidates the game session schedule query cache.
+   *
+   * @returns A mutation hook that deletes a game session schedule.
+   */
+  useDeleteGameSessionSchedule: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: AdminGameSessionScheduleService.deleteGameSessionSchedule,
+      // TODO: when get by id is implemented, only invalidate the updated id for get by id
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.GAME_SESSION_SCHEDULE_QUERY_KEY],
+        }),
     })
   },
 } as const
--- apps/frontend/src/services/admin/user/AdminUserQuery.ts
+++ apps/frontend/src/services/admin/user/AdminUserQuery.ts
@@ -1,9 +1,26 @@
-import type { PaginationQuery } from "@repo/shared"
-import { useInfiniteQuery } from "@tanstack/react-query"
+import type { PaginationQuery, UpdateUserRequest } from "@repo/shared"
+import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query"
 import { QueryKeys } from "@/services"
 import AdminUserService from "./AdminUserService"
 
 const AdminUserQuery = {
+  /**
+   * Provides a mutation hook for creating a new user and invalidates the user query cache.
+   *
+   * @returns A mutation hook that creates a user.
+   */
+  useCreateUser: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: AdminUserService.createUser,
+      // TODO: when get by id is implemented, do not invalidate get by id queries
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.USER_QUERY_KEY],
+        }),
+    })
+  },
+
   /**
    * Retrieves and caches paginated users.
    *
@@ -23,6 +40,43 @@ const AdminUserQuery = {
       },
       getNextPageParam: (lastPage) => lastPage?.data?.nextPage ?? undefined,
       getPreviousPageParam: (firstPage) => firstPage?.data?.prevPage ?? undefined,
+    })
+  },
+
+  /**
+   * Provides a mutation hook for updating a user and invalidates the user query cache.
+   *
+   * @returns A mutation hook that updates a user.
+   */
+  useUpdateUser: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: async ({ id, userData }: { id: string; userData: UpdateUserRequest }) => {
+        const response = await AdminUserService.updateUser(id, userData)
+        return response
+      },
+      // TODO: When get by id is implemented, only invalidate the one id for get by id or update the specific query with the updated data
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.USER_QUERY_KEY],
+        }),
+    })
+  },
+
+  /**
+   * Provides a mutation hook for deleting a user and invalidates the user query cache.
+   *
+   * @returns A mutation hook for deleting a user.
+   */
+  useDeleteUser: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: AdminUserService.deleteUser,
+      // TODO: When get by id is implemented, only invalidate the one id for get by id
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.USER_QUERY_KEY],
+        }),
     })
   },
 } as const
