da7a6af18ca248a5f15c4b0618de63fc3faa2042
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-19 12:02:32 +1200

    revert: undo gameSessionSchedule.mock import change (Windows can't see file name case changes)

--- packages/shared/src/mocks/index.ts
+++ packages/shared/src/mocks/index.ts
@@ -1,5 +1,5 @@
 export * from "./Booking.mock"
 export * from "./GameSession.mock"
-export * from "./GameSessionSchedule.mock"
+export * from "./gameSessionSchedule.mock"
 export * from "./Semester.mock"
 export * from "./User.mock"
