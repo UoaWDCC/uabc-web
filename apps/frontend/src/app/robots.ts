7b203b6c77ab3550b56af60e9218c984109f2a23
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-19 16:23:29 +1200

    feat(robots, sitemap): add dynamic export for force-static rendering in robots and sitemap files

--- apps/frontend/src/app/robots.ts
+++ apps/frontend/src/app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next"
 
+export const dynamic = "force-static"
+
 export default function robots(): MetadataRoute.Robots {
   if (process.env.NODE_ENV === "production") {
     return {
