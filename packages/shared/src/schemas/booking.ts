c39b09ae131e407b5806aa5756234f07b2fe754f
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-07-19 23:56:55 +1200

    refactor: move user schema import

--- packages/shared/src/schemas/booking.ts
+++ packages/shared/src/schemas/booking.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 import { type CreateBookingData, PlayLevel } from "../types"
-import { UserSchema } from "./auth"
 import { GameSessionSchema } from "./game-session"
+import { UserSchema } from "./user"
 
 export const BookingSchema = z.object({
   user: z.union([z.string(), UserSchema]),
