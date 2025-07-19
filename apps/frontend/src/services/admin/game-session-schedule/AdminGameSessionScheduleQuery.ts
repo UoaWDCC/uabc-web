8aefba2684f24ff829df1d0e14c93c41a6df2bb2
mikais13 <mikai.somerville@gmail.com>
2025-07-20 01:43:51 +1200

    feat(services): create frontend mutations for create, update, and delete services

---
+++ apps/frontend/src/services/admin/game-session-schedule/AdminGameSessionScheduleQuery.ts
@@ -0,0 +1,71 @@
+import type { UpdateGameSessionScheduleRequest } from "@repo/shared"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { QueryKeys } from "@/services"
+import AdminGameSessionScheduleService from "./AdminGameSessionScheduleService"
+
+const AdminGameSessionScheduleQuery = {
+  /**
+   * Provides a mutation hook for creating a new game session schedule and invalidates the game session schedule query cache.
+   *
+   * @returns A mutation hook that creates a game session schedule.
+   */
+  useCreateGameSessionSchedule: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: AdminGameSessionScheduleService.createGameSessionSchedule,
+      // TODO: when get by id is implemented, do not invalidate get by id queries
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.GAME_SESSION_SCHEDULE_QUERY_KEY],
+        }),
+    })
+  },
+
+  /**
+   * Provides a mutation hook for updating a game session schedule and invalidates the game session schedule query cache.
+   *
+   * @returns A mutation hook that updates a game session schedule.
+   */
+  useUpdateGameSessionSchedule: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: async ({
+        id,
+        gameSessionScheduleData,
+      }: {
+        id: string
+        gameSessionScheduleData: UpdateGameSessionScheduleRequest
+      }) => {
+        const response = await AdminGameSessionScheduleService.updateGameSessionSchedule(
+          id,
+          gameSessionScheduleData,
+        )
+        return response
+      },
+      // TODO: when get by id is implemented, only invalidate the updated id for get by id, or update the specific query with the updated data
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.GAME_SESSION_SCHEDULE_QUERY_KEY],
+        }),
+    })
+  },
+
+  /**
+   * Provides a mutation hook for deleting a game session schedule and invalidates the game session schedule query cache.
+   *
+   * @returns A mutation hook that deletes a game session schedule.
+   */
+  useDeleteGameSessionSchedule: () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+      mutationFn: AdminGameSessionScheduleService.deleteGameSessionSchedule,
+      // TODO: when get by id is implemented, only invalidate the updated id for get by id
+      onSuccess: () =>
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.GAME_SESSION_SCHEDULE_QUERY_KEY],
+        }),
+    })
+  },
+} as const
+
+export default AdminGameSessionScheduleQuery
