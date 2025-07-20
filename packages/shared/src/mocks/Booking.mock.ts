2bbcf09c3e6c5237b69119161623b879592c48f4
Brandon Seng Han Chan <160544499+br-Chan@users.noreply.github.com>
2025-07-20 12:23:44 +1200

    feat(profile): implement user profile page (#571)
    * feat: add user panel, create ProfileBookingPanel for /profile; copy mocks from backend into shared
    
    * feat: add ProfileDetails to /profile
    
    * feat: add default values to ProfileDetails and values to user mock; format booking card time range
    
    * fix: add fields to user schema
    
    * fix: correct the UserSchema and hard-code the enum values (backend tests now pass)
    
    * chore: add JSDoc comments to /profile and ProfileBookingPanel
    
    * fix: fix various typing issues and fix BookingCard test
    
    * chore: fix  import order
    
    * feat: add tests for /profile
    
    * feat: wrap QueryClientProvider around <Profile /> in tests
    
    * fix: use getByText to get strings in tests
    
    * fix: amend expect statements in /profile tests
    
    * fix: swap the value and key in AdditionalInfo select options
    
    * refactor: remove title case logic from AdditionalInfo select options
    
    * revert: undo title case removal and hard code test case variables
    
    * fix: change enums in AdditionalInfo and fix up a test value for /profile
    
    * fix: hard code more test case values
    
    * fix: get text in inputs by display value
    
    * fix: remove h="full" from user panel so height does not follow height of bookings panel
    
    * revert: undo gameSessionSchedule.mock import change (Windows can't see file name case changes)
    
    * refactor: make times fixed in game session mock
    
    * feat: implement responsive design for user panel and profile booking panel
    
    * fix: remove enum value manipulation from AdditionalInfo; pass user into UserPanel
    
    * chore: run lint
    
    * fix: make all `/profile` page tests case variables programmatic based on casualUserMock
    
    * chore: run lint
    
    * fix: remove undefined  from AdditionalInfo defaultValues
    
    * fix: check if gender and play level are truthy before checking existence in document
    
    * chore: run lint

---
+++ packages/shared/src/mocks/Booking.mock.ts
@@ -0,0 +1,38 @@
+import type { Booking } from "../payload-types"
+import { gameSessionMock } from "./GameSession.mock"
+import { casualUserMock } from "./User.mock"
+
+export const bookingsMock: Booking[] = [
+  {
+    id: "ccaf8f75ceb9f059773d4774",
+    user: casualUserMock,
+    gameSession: gameSessionMock,
+    playerLevel: "beginner",
+    updatedAt: new Date(2025, 0, 1).toISOString(),
+    createdAt: new Date(2025, 0, 1).toISOString(),
+  },
+  {
+    id: "ccaf8f75ceb9f059773d4775",
+    user: casualUserMock,
+    gameSession: gameSessionMock,
+    playerLevel: "beginner",
+    updatedAt: new Date(2025, 0, 1).toISOString(),
+    createdAt: new Date(2025, 0, 1).toISOString(),
+  },
+  {
+    id: "ccaf8f75ceb9f059773d4776",
+    user: casualUserMock,
+    gameSession: gameSessionMock,
+    playerLevel: "beginner",
+    updatedAt: new Date(2025, 0, 1).toISOString(),
+    createdAt: new Date(2025, 0, 1).toISOString(),
+  },
+  {
+    id: "ccaf8f75ceb9f059773d4777",
+    user: casualUserMock,
+    gameSession: gameSessionMock,
+    playerLevel: "beginner",
+    updatedAt: new Date(2025, 0, 1).toISOString(),
+    createdAt: new Date(2025, 0, 1).toISOString(),
+  },
+]
