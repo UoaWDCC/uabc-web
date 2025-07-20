5291649722f00cc8497c820300654ebd54600df4
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-20 22:56:56 +1200

    feat(footer): make footer section fetched during build

--- packages/ui/src/components/Generic/Footer/FooterBrand.tsx
+++ packages/ui/src/components/Generic/Footer/FooterBrand.tsx
@@ -1,14 +1,18 @@
 import { UabcLogo } from "@repo/ui/components/Icon"
-import { Heading } from "@repo/ui/components/Primitive"
+import { Heading, Image } from "@repo/ui/components/Primitive"
 import type { StackProps } from "@yamada-ui/react"
 import { HStack, Text, VStack } from "@yamada-ui/react"
-import type { ReactNode } from "react"
 import { memo } from "react"
 import type { SocialLink } from "./constants"
 import { FooterSocialLinks } from "./FooterSocialLinks"
 
 export interface FooterBrandProps extends StackProps {
-  logo?: ReactNode
+  logo?: {
+    url: string
+    alt: string
+    width: number
+    height: number
+  }
   title: string
   description: string
   socialLinks: SocialLink[]
@@ -19,8 +23,19 @@ export const FooterBrand = memo<FooterBrandProps>(
     return (
       <VStack display={{ base: "none", md: "flex" }} gap="lg" {...props}>
         <VStack gap="xs">
-          <HStack>
-            {logo ?? <UabcLogo />}
+          <HStack alignItems="center">
+            {logo?.url ? (
+              <Image
+                alt={logo.alt}
+                h="12"
+                height={logo.height ?? 200}
+                src={logo.url}
+                w="12"
+                width={logo.width ?? 200}
+              />
+            ) : (
+              <UabcLogo />
+            )}
             <Heading.h2 fontSize="6xl" fontWeight="semibold">
               {title}
             </Heading.h2>
