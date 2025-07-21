966b40692a00ce4cc53fef846f681338c8147632
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-07-21 17:41:25 +1200

    refactor: rename UserInfoResponseSchema to GoogleUserInfo

--- packages/shared/src/types/auth/login.ts
+++ packages/shared/src/types/auth/login.ts
@@ -1,11 +1,11 @@
 import type z from "zod"
 import type {
+  GoogleUserInfoResponseSchema,
   JWTEncryptedUserSchema,
   LoginFormDataSchema,
   LoginRequestBodySchema,
   LoginResponseSchema,
   RegisterRequestBodySchema,
-  UserInfoResponseSchema,
 } from "../../schemas"
 
 export const AUTH_COOKIE_NAME = "auth_token"
@@ -13,7 +13,7 @@ export const STATE_COOKIE_NAME = "state"
 export const TOKEN_EXPIRY_TIME = "1h"
 
 export type JWTEncryptedUser = z.infer<typeof JWTEncryptedUserSchema>
-export type UserInfoResponse = z.infer<typeof UserInfoResponseSchema>
+export type GoogleUserInfo = z.infer<typeof GoogleUserInfoResponseSchema>
 export type LoginRequestBody = z.infer<typeof LoginRequestBodySchema>
 export type LoginResponse = z.infer<typeof LoginResponseSchema>
 export type LoginFormData = z.infer<typeof LoginFormDataSchema>
