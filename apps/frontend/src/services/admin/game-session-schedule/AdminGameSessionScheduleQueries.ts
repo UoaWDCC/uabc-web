93fc9e2ad3d4f8baad82d44320f100eec7cb38ee
mikais13 <mikai.somerville@gmail.com>
2025-07-22 21:56:29 +1200

    refactor(queries): export queries individually and use consistent ...Queries.ts file names

---
+++ apps/frontend/src/services/admin/game-session-schedule/AdminGameSessionScheduleQueries.ts
@@ -0,0 +1,26 @@
+import type { PaginationQuery } from "@repo/shared"
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { QueryKeys } from "@/services"
+import AdminGameSessionScheduleService from "./AdminGameSessionScheduleService"
+
+/**
+ * Retrieves and caches paginated game session schedules.
+ *
+ * @param query The pagination query parameters.
+ * @returns A query hook that fetches all game session schedules.
+ */
+export const useGetPaginatedGameSessionSchedules = (query: PaginationQuery) => {
+  return useInfiniteQuery({
+    queryKey: [QueryKeys.GAME_SESSION_SCHEDULE_QUERY_KEY],
+    initialPageParam: 1,
+    queryFn: async ({ pageParam }) => {
+      const response = await AdminGameSessionScheduleService.getAllPaginatedGameSessionSchedules({
+        ...query,
+        page: pageParam,
+      })
+      return response
+    },
+    getNextPageParam: (lastPage) => lastPage?.data?.nextPage,
+    getPreviousPageParam: (firstPage) => firstPage?.data?.prevPage,
+  })
+}
