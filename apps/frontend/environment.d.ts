d8e8262501ec1c356b232d90fadc17e175126b78
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-25 11:19:59 +1200

    fix(turbo): update build dependencies in turbo.json for frontend tasks

--- apps/frontend/environment.d.ts
+++ apps/frontend/environment.d.ts
@@ -9,6 +9,26 @@ declare global {
        * The public URL of the backend
        */
       NEXT_PUBLIC_API_URL: string
+      /**
+       * The SMTP host
+       */
+      SMTP_HOST: string
+      /**
+       * The SMTP user
+       */
+      SMTP_USER: string
+      /**
+       * The SMTP password
+       */
+      SMTP_PASS: string
+      /**
+       * The payload secret
+       */
+      PAYLOAD_SECRET: string
+      /**
+       * The database URI
+       */
+      DATABASE_URI: string
     }
   }
 }
