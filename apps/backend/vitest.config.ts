68715412933d35361ce6e8103912397aacb5a3fe
Shintaro Jokagi <61367823+taroj1205@users.noreply.github.com>
2025-07-25 15:06:27 +1200

    feat(payload): make admin routes root page (#661)

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -5,5 +5,14 @@ export default mergeConfig(backendConfig, {
   test: {
     // You can add backend-specific overrides here
     setupFiles: ["./src/test-config/vitest.setup.ts"],
+    coverage: {
+      exclude: [
+        "**/test-config/**",
+        "**/app/layout.tsx",
+        "**/app/payload/**",
+        "**/app/[[...segments]]/**",
+        "**/app/importMap.js",
+      ],
+    },
   },
 })
