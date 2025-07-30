58c3483ecd699eef109df40c4603cbe27cfc6cdc
Shintaro Jokagi <61367823+taroj1205@users.noreply.github.com>
2025-07-30 13:38:38 +0900

    feat(bookings): implement booking open day and time checks (#650)

--- packages/shared/src/mocks/GameSession.mock.ts
+++ packages/shared/src/mocks/GameSession.mock.ts
@@ -1,15 +1,22 @@
 import type { GameSession } from "../payload-types"
 import { gameSessionScheduleMock } from "./gameSessionSchedule.mock"
-import { semesterMock } from "./Semester.mock"
+import { semesterMock, semesterMockBookingNotOpen } from "./Semester.mock"
 
 export const gameSessionMock: GameSession = {
   id: "87efbe48887bc7ae09e305ed",
   semester: semesterMock,
-  startTime: new Date(2025, 6, 19, 9, 0, 0).toISOString(),
-  endTime: new Date(2025, 6, 19, 11, 0, 0).toISOString(),
+  startTime: new Date(Date.UTC(2025, 6, 19, 9, 0, 0)).toISOString(),
+  endTime: new Date(Date.UTC(2025, 6, 19, 11, 0, 0)).toISOString(),
   gameSessionSchedule: gameSessionScheduleMock,
   capacity: 10,
   casualCapacity: 8,
-  updatedAt: new Date(2025, 0, 1).toISOString(),
-  createdAt: new Date(2025, 0, 1).toISOString(),
+  updatedAt: new Date(Date.UTC(2025, 0, 1)).toISOString(),
+  createdAt: new Date(Date.UTC(2025, 0, 1)).toISOString(),
+}
+
+export const gameSessionMockBookingNotOpen: GameSession = {
+  ...gameSessionMock,
+  semester: semesterMockBookingNotOpen,
+  startTime: new Date(Date.UTC(2025, 6, 21, 9, 0, 0)).toISOString(), // Monday July 21, 2025 at 9am UTC
+  endTime: new Date(Date.UTC(2025, 6, 21, 11, 0, 0)).toISOString(), // Monday July 21, 2025 at 11am UTC
 }
