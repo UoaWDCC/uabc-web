41e6ea35236f0517abcd315b59e7488a80629e94
Shintaro Jokagi <taroj1205@gmail.com>
2025-08-05 11:13:04 +1200

    test(BookACourt): enhance tests by passing mock onSelect prop

--- packages/ui/src/components/Generic/BookACourt/BookACourt.test.tsx
+++ packages/ui/src/components/Generic/BookACourt/BookACourt.test.tsx
@@ -5,13 +5,15 @@ import { render, screen } from "@repo/ui/test-utils"
 import { isValidElement } from "react"
 
 describe("<BookACourt />", () => {
+  const mockOnSelect = vi.fn()
+
   it("should re-export BookACourt component, and check it exists", () => {
     expect(BookACourtModule.BookACourt).toBeDefined()
-    expect(isValidElement(<BookACourt />)).toBeTruthy()
+    expect(isValidElement(<BookACourt onSelect={mockOnSelect} />)).toBeTruthy()
   })
 
   it("should render all play level buttons as links with correct href", () => {
-    render(<BookACourt />)
+    render(<BookACourt onSelect={mockOnSelect} />)
 
     Object.values(PlayLevel).forEach((level) => {
       const levelLink = screen.getByRole("link", {
@@ -23,7 +25,7 @@ describe("<BookACourt />", () => {
   })
 
   it("should render guideline link call guidelineOnClick when the guideline link is clicked", () => {
-    render(<BookACourt />)
+    render(<BookACourt onSelect={mockOnSelect} />)
 
     const guidelineLink = screen.getByRole("link", { name: "Check-In Form Guidelines" })
     expect(guidelineLink).toBeInTheDocument()
