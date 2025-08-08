7fe1494809e95f9b5047efe1364128cb457f4edd
Brandon Seng Han Chan <160544499+br-Chan@users.noreply.github.com>
2025-08-08 20:58:56 +1200

    feat (register-flow): create Register Flow (#672)
    * feat: create CasualInfoFormSchema and type for register flow state
    
    * feat: add steps array for all forms in flow, with default values in each component
    
    * feat: add story and empty test file
    
    * feat: implement back button and placeholder title
    
    * feat: implement dynamically changing title in register flow
    
    * feat: implement tests for the RegisterFlow and move over local storage test utils
    
    * fix: get by display value instead of text when testing for default values
    
    * fix: add use client to the RegisterFlow component
    
    * fix:  make corrections to the keys in the steps array
    
    * refactor: define RegisterFlowState using RegisterFlowStateSchema instead
    
    * fix: add 10,000 ms timeout value to long register flow test
    
    * refactor: make last changes

---
+++ packages/ui/src/components/Generic/RegisterFlow/RegisterFlow.tsx
@@ -0,0 +1,202 @@
+"use client"
+
+import { RegisterFlowStateSchema } from "@repo/shared/schemas"
+import type {
+  AdditionalInfoFormValues,
+  BasicInfoForm1Values,
+  BasicInfoForm2Values,
+  CasualInfoFormValues,
+  RegisterFlowState,
+  UniversityInfoFormValues,
+} from "@repo/shared/types"
+import { useLocalStorage } from "@repo/ui/hooks"
+import { ArrowLeftIcon } from "@yamada-ui/lucide"
+import { Center, Grid, GridItem, VStack } from "@yamada-ui/react"
+import { memo, useReducer } from "react"
+import { Heading, IconButton, LoadingStateBar } from "../../Primitive"
+import { AdditionalInfoForm } from "./AdditionalInfoForm"
+import { BasicInfoForm1 } from "./BasicInfoForm1"
+import { BasicInfoForm2 } from "./BasicInfoForm2"
+import { CasualInfoForm } from "./CasualInfoForm"
+import { RegisterSuccessPanel } from "./RegisterSuccessPanel"
+import { UniversityInfoForm } from "./UniversityInfoForm"
+
+const initialState = {
+  step: 0,
+  basicInfo1: null,
+  basicInfo2: null,
+  universityInfo: null,
+  additionalInfo: null,
+  casualInfo: null,
+} satisfies RegisterFlowState
+
+type RegisterFlowActionPayloads = {
+  SET_BASIC_INFO_1: BasicInfoForm1Values
+  SET_BASIC_INFO_2: BasicInfoForm2Values
+  SET_UNIVERSITY_INFO: UniversityInfoFormValues
+  SET_ADDITIONAL_INFO: AdditionalInfoFormValues
+  SET_CASUAL_INFO: CasualInfoFormValues
+}
+
+type RegisterFlowAction =
+  | { type: "NEXT" }
+  | { type: "PREV" }
+  | {
+      [K in keyof RegisterFlowActionPayloads]: {
+        type: K
+        payload: RegisterFlowActionPayloads[K]
+      }
+    }[keyof RegisterFlowActionPayloads]
+
+const reducer = (state: RegisterFlowState, action: RegisterFlowAction): RegisterFlowState => {
+  switch (action.type) {
+    case "NEXT":
+      return {
+        ...state,
+        step: state.step + 1,
+      }
+    case "PREV":
+      return {
+        ...state,
+        step: state.step - 1,
+      }
+    case "SET_BASIC_INFO_1":
+      return {
+        ...state,
+        basicInfo1: action.payload,
+      }
+    case "SET_BASIC_INFO_2":
+      return {
+        ...state,
+        basicInfo2: action.payload,
+      }
+    case "SET_UNIVERSITY_INFO":
+      return {
+        ...state,
+        universityInfo: action.payload,
+      }
+    case "SET_ADDITIONAL_INFO":
+      return {
+        ...state,
+        additionalInfo: action.payload,
+      }
+    case "SET_CASUAL_INFO":
+      return {
+        ...state,
+        casualInfo: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+const LOCAL_STORAGE_KEY = "register-flow"
+
+export const RegisterFlow = memo(() => {
+  const { value: persistedState, setValue: setPersistedState } = useLocalStorage<RegisterFlowState>(
+    LOCAL_STORAGE_KEY,
+    RegisterFlowStateSchema,
+  )
+
+  const [state, dispatch] = useReducer(reducer, persistedState ?? initialState)
+
+  const handleStepSubmit = <K extends keyof RegisterFlowActionPayloads>(type: K) => {
+    return (payload: RegisterFlowActionPayloads[K]) => {
+      const action = { type, payload } as RegisterFlowAction
+      dispatch(action)
+
+      const nextState = reducer(state, action)
+      setPersistedState(nextState)
+      dispatch({ type: "NEXT" })
+    }
+  }
+
+  const handleGoBack = () => {
+    dispatch({ type: "PREV" })
+    const nextState = reducer(state, { type: "PREV" })
+    setPersistedState(nextState)
+  }
+
+  const steps = [
+    {
+      title: "Basic Info",
+      element: (
+        <BasicInfoForm1
+          defaultValues={state.basicInfo1 ?? undefined}
+          key="basic-info-form-1"
+          onSubmit={handleStepSubmit("SET_BASIC_INFO_1")}
+        />
+      ),
+    },
+    {
+      title: "Basic Info",
+      element: (
+        <BasicInfoForm2
+          defaultValues={state.basicInfo2 ?? undefined}
+          key="basic-info-form-2"
+          onSubmit={handleStepSubmit("SET_BASIC_INFO_2")}
+        />
+      ),
+    },
+    {
+      title: "University Info",
+      element: (
+        <UniversityInfoForm
+          defaultValues={state.universityInfo ?? undefined}
+          key="university-info-form-1"
+          onSubmit={handleStepSubmit("SET_UNIVERSITY_INFO")}
+        />
+      ),
+    },
+    {
+      title: "Additional Info",
+      element: (
+        <AdditionalInfoForm
+          defaultValues={state.additionalInfo ?? undefined}
+          key="additional-info-form-1"
+          onSubmit={handleStepSubmit("SET_ADDITIONAL_INFO")}
+        />
+      ),
+    },
+    {
+      title: "Casual Member Info",
+      element: (
+        <CasualInfoForm
+          defaultValues={state.casualInfo ?? undefined}
+          key="casual-info-form-1"
+          onSubmit={handleStepSubmit("SET_CASUAL_INFO")}
+        />
+      ),
+    },
+  ]
+
+  const enableGoBack = state.step > 0 && state.step < steps.length
+
+  return (
+    <VStack>
+      <Grid templateColumns="repeat(3, 1fr)">
+        <GridItem>
+          <IconButton
+            aria-label="Go back"
+            color={["black", "white"]}
+            data-testid="go-back"
+            disabled={!enableGoBack}
+            icon={<ArrowLeftIcon />}
+            onClick={handleGoBack}
+            // rounded="full"
+            size="sm"
+            variant="ghost"
+          />
+        </GridItem>
+
+        <GridItem as={Center}>
+          <Heading.h2>{steps[state.step]?.title}</Heading.h2>
+        </GridItem>
+      </Grid>
+      <LoadingStateBar value={(state.step / steps.length) * 100} />
+      {steps[state.step]?.element ?? <RegisterSuccessPanel />}
+    </VStack>
+  )
+})
+
+RegisterFlow.displayName = "RegisterFlow"
