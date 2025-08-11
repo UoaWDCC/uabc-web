fb85581bfe14c217018f5884985b230e2ffa0e3c
mikais13 <mikai.somerville@gmail.com>
2025-08-11 15:01:32 +1200

    feat(theme): allow notifications to be closed

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,12 +1,16 @@
 import type { ThemeConfig, UsageTheme } from "@yamada-ui/core"
 import { extendConfig, extendTheme } from "@yamada-ui/theme-tools"
 import { components } from "./components"
-import { breakpoint, initialColorMode } from "./config"
+import { breakpoint, initialColorMode, noticeOptions } from "./config"
 import { semantics } from "./semantics"
 import { styles } from "./styles"
 import { tokens } from "./tokens"
 
-export const defaultConfig: ThemeConfig = { breakpoint, initialColorMode }
+export const defaultConfig: ThemeConfig = {
+  breakpoint,
+  initialColorMode,
+  notice: { options: noticeOptions },
+}
 
 export const config = extendConfig(defaultConfig)
 
