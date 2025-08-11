7144a09102b7841984c9f03382633cf6907eeba4
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-08-12 11:51:52 +1200

    feat: add queried user data array into admin table

--- apps/frontend/src/components/client/admin/AdminMembersSection.tsx
+++ apps/frontend/src/components/client/admin/AdminMembersSection.tsx
@@ -1,100 +1,23 @@
-import { PlayLevel } from "@repo/shared/types"
-import { AdminTable } from "@repo/ui/components/Composite"
+import { AdminTable, type UserData } from "@repo/ui/components/Composite"
 import { useGetPaginatedUsers } from "@/services/admin/user/AdminUserQueries"
 
 export const AdminMembersSection = () => {
-  const thing = useGetPaginatedUsers({
+  const { data } = useGetPaginatedUsers({
     limit: 20,
     page: 20,
   })
 
-  const mockData = [
-    {
-      id: "1",
-      name: "Alice Smith",
-      email: "alice@example.com",
-      role: "admin",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "University of Auckland",
-      level: PlayLevel.beginner,
-    },
-    {
-      id: "2",
-      name: "Bob Smith",
-      email: "bob@example.com",
-      role: "casual",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "Auckland University of Technology",
-      level: PlayLevel.advanced,
-    },
-    {
-      id: "3",
-      name: "Charlie Smith",
-      email: "charlie@example.com",
-      role: "member",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "Massey University",
-      level: PlayLevel.intermediate,
-    },
-    {
-      id: "4",
-      name: "David Smith",
-      email: "david@example.com",
-      role: "casual",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "University of Auckland",
-      level: PlayLevel.beginner,
-    },
-    {
-      id: "5",
-      name: "Eve Smith",
-      email: "eve@example.com",
-      role: "member",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "Auckland University of Technology",
-      level: PlayLevel.advanced,
-    },
-    {
-      id: "6",
-      name: "Frank Smith",
-      email: "frank@example.com",
-      role: "casual",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "Massey University",
-      level: PlayLevel.intermediate,
-    },
-    {
-      id: "7",
-      name: "Grace Smith",
-      email: "grace@example.com",
-      role: "member",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "University of Auckland",
-      level: PlayLevel.beginner,
-    },
-    {
-      id: "8",
-      name: "Hank Smith",
-      email: "hank@example.com",
-      role: "casual",
-      remaining: "10",
-      joined: "2021-01-01",
-      university: "Auckland University of Technology",
-      level: PlayLevel.advanced,
-    },
-  ]
+  const userData =
+    data?.pages[0].data.docs.map((page) => ({
+      id: page.id,
+      name: page.firstName + page.lastName,
+      email: page.email,
+      remaining: String(page.remainingSessions),
+      joined: page.createdAt,
+      role: page.role,
+      university: "todo",
+      level: page.playLevel,
+    })) ?? []
 
-  console.log("pages:")
-  thing.data?.pages.map((page) => {
-    console.log(page)
-  })
-
-  return <AdminTable data={mockData} />
+  return <AdminTable data={userData as UserData[]} />
 }
