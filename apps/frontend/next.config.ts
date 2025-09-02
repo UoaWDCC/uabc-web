3e7eb91bffb8f7a6e08931889452aa7fc737f7bc
Shintaro Jokagi <taroj1205@gmail.com>
2025-09-02 12:19:53 +1200

    chore(frontend): remove OpenNext Cloudflare integration and update deployment configurations

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,41 +1,31 @@
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare"
 import type { NextConfig } from "next"
 import type { RemotePattern } from "next/dist/shared/lib/image-config"
 
-const env = process.env.NEXT_CONFIG_ENV || "development"
+const remotePatterns: RemotePattern[] = [
+  // TODO: remove once actual images implemented
+  {
+    protocol: "https",
+    hostname: "images.unsplash.com",
+  },
+]
 
-const config = (async () => {
-  if (env === "development") {
-    initOpenNextCloudflareForDev()
-  }
+if (process.env.NEXT_PUBLIC_API_URL) {
+  const { protocol, hostname, port, pathname } = new URL(process.env.NEXT_PUBLIC_API_URL)
+  remotePatterns.push({
+    protocol: protocol.replace(":", "") as "http" | "https",
+    hostname,
+    port,
+    pathname: `${pathname.replace(/\/$/, "")}/payload/api/media/**`,
+  })
+}
 
-  const remotePatterns: RemotePattern[] = [
-    // TODO: remove once actual images implemented
-    {
-      protocol: "https",
-      hostname: "images.unsplash.com",
-    },
-  ]
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns,
+  },
+  output: "export",
+  trailingSlash: true,
+  distDir: "out",
+}
 
-  if (process.env.NEXT_PUBLIC_API_URL) {
-    const { protocol, hostname, port, pathname } = new URL(process.env.NEXT_PUBLIC_API_URL)
-    remotePatterns.push({
-      protocol: protocol.replace(":", "") as "http" | "https",
-      hostname,
-      port,
-      pathname: `${pathname.replace(/\/$/, "")}/payload/api/media/**`,
-    })
-  }
-
-  const nextConfig: NextConfig = {
-    images: {
-      remotePatterns,
-    },
-    output: "export",
-    trailingSlash: true,
-  }
-
-  return nextConfig
-})()
-
-export default config
+export default nextConfig
