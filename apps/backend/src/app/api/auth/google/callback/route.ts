350df9dc1306dc7864dfbf2c027d0b0a50511d67
Mikai Somerville <91355759+mikais13@users.noreply.github.com>
2025-09-05 14:34:50 +1200

    feat(verification-code): add a expiration and resending to verification codes (#722)
    * feat(register, verification-code): allow resending of verification codes
    
    * fix(schemas): make email verification code id optional
    
    * feat(mail): include expiration details in verification code email
    
    * fix(mail): update test to new email content
    
    * chore(verification-code): remove commented code
    
    * refactor(register): use simple .some syntax for checking code validity
    
    * feat(verification-code): use 10 minutes for expiry time
    
    * refactor(verification-code): use a date util function to get verification code expiry
    
    * fix(verification-code): only allow latest code to work and add 5 minute cool down
    
    * test(date-utils): test get verification code cool down date, use improved test cases in expiry date
    
    * chore(services): update email verification content with 10min expiry
    
    * chore(verification-code): improve error response for cooldown period
    
    * fix(user): require email verification code field
    
    * chore(user): remove optional chaining of email verification attribute access
    
    * refactor(mocks): unify verification code mock usage across tests
    
    * refactor(user): use a single object for verification code
    
    * fix(user): use correct email verification structure for onboarded user schema
    
    * chore(me): remove unnecessary comment
    
    * refactor(user): remove default value and use interface name for email verification

--- apps/backend/src/app/api/auth/google/callback/route.ts
+++ apps/backend/src/app/api/auth/google/callback/route.ts
@@ -1,4 +1,5 @@
 import { GoogleUserInfoResponseSchema, MembershipType, TOKEN_EXPIRY_TIME } from "@repo/shared"
+import { VERIFICATION_CODE_MOCK } from "@repo/shared/mocks"
 import type { User } from "@repo/shared/payload-types"
 import { StatusCodes } from "http-status-codes"
 import { cookies } from "next/headers"
@@ -96,6 +97,11 @@ export const GET = async (req: NextRequest) => {
         lastName: family_name,
         role: MembershipType.casual,
         email,
+        emailVerification: {
+          verificationCode: VERIFICATION_CODE_MOCK,
+          expiresAt: new Date(1970, 1, 1).toISOString(),
+          createdAt: new Date(1970, 1, 1).toISOString(),
+        },
       })
     } else {
       throw error
