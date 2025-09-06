4ad0bd41f6b35f6639f5d493f7190c2bb68b41fe
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-09-07 11:45:57 +1200

    refactor: change joined column cell to not use Text and just use typical getValue

--- packages/ui/src/components/Composite/AdminTable/Columns.tsx
+++ packages/ui/src/components/Composite/AdminTable/Columns.tsx
@@ -1,5 +1,4 @@
 import type { PlayLevel } from "@repo/shared"
-import { Text } from "@yamada-ui/react"
 import type { Column } from "@yamada-ui/table"
 
 export type UserData = {
@@ -65,7 +64,7 @@ export const columns: Column<UserData>[] = [
   {
     accessorKey: "joined",
     header: "Joined",
-    cell: ({ getValue }) => <Text as="span">{getValue<string>()}</Text>,
+    cell: ({ getValue }) => <>{getValue<string>()}</>,
     css: {
       minW: "xs",
     },
