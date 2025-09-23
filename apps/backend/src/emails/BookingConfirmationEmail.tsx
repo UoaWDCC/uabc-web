c0c3662ee07b21f838e9e3f8aa522fb225ee31ea
mikais13 <mikai.somerville@gmail.com>
2025-09-23 17:25:08 +1200

    feat(emails): add tailwind styles to booking confirmation email

--- apps/backend/src/emails/BookingConfirmationEmail.tsx
+++ apps/backend/src/emails/BookingConfirmationEmail.tsx
@@ -1,4 +1,17 @@
-import { Body, Head, Heading, Html, Preview, Text } from "@react-email/components"
+import {
+  Body,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components"
+import { TailwindConfig } from "./_components/TailwindConfig"
 
 export interface BookingConfirmationEmailProps {
   weekday: string
@@ -15,25 +28,47 @@ const BookingConfirmationEmail = ({
 }: BookingConfirmationEmailProps) => {
   return (
     <Html>
-      <Head title="UABC Booking Confirmation" />
-      <Body>
-        <Preview>
-          Your booking for our {weekday} session at {sessionName} has been confirmed!
-        </Preview>
-        <Heading>Booking Confirmation</Heading>
-        <Text>
-          Your booking for our {weekday} session at {sessionName} has been confirmed!
-        </Text>
-        <Text>The details for your session are as follows:</Text>
-        <ul>
-          <li>Date: {weekday}</li>
-          <li>Time: {time}</li>
-          {sessionLocation && <li>Location: {sessionLocation}</li>}
-        </ul>
-        <Text>We'll see you on the courts! 🏸</Text>
-        <Text>Kind regards,</Text>
-        <Text>UABC Team</Text>
-      </Body>
+      <TailwindConfig>
+        <Head title="UABC Booking Confirmation">
+          <Font
+            fallbackFontFamily="Arial"
+            fontFamily="Geist"
+            webFont={{
+              url: "https://fonts.googleapis.com/css2?family=Geist:wght@100;200;300;400;500;600;700;800;900&display=swap",
+              format: "woff2",
+            }}
+          />
+        </Head>
+        <Body className="rounded-2xl bg-white font-medium font-sans text-md">
+          <Preview>
+            Your booking for our {weekday} session at {sessionName} has been confirmed!
+          </Preview>
+          <Container className="m-0 w-full max-w-2xl p-4">
+            <Heading className="m-0 text-5xl text-primary">Booking Confirmation</Heading>
+            <Hr className="my-4" style={{ borderTopColor: "#303030" }} />
+            <Section>
+              <Text className="m-0 mb-2 font-medium text-md">
+                Your booking for our {weekday} session at {sessionName} has been confirmed!
+              </Text>
+            </Section>
+            <Section className="my-2">
+              <Row className="my-1 font-medium text-md">
+                The details for your session are as follows:
+              </Row>
+              <ul className="m-0 list-disc">
+                <li className="my-0.5">Date: {weekday}</li>
+                <li className="my-0.5">Time: {time}</li>
+                {sessionLocation && <li className="my-0.5">Location: {sessionLocation}</li>}
+              </ul>
+            </Section>
+            <Text className="my-4 font-medium text-md">We'll see you on the courts! 🏸</Text>
+            <Section className="my-4 gap-0">
+              <Text className="m-0 font-medium text-md">Kind regards,</Text>
+              <Text className="m-0 font-medium text-md">UABC Team</Text>
+            </Section>
+          </Container>
+        </Body>
+      </TailwindConfig>
     </Html>
   )
 }
@@ -43,6 +78,6 @@ BookingConfirmationEmail.PreviewProps = {
   time: "18:30",
   sessionName: "UoA Rec Center",
   sessionLocation: "UoA Rec Center, 123 University Rd, City",
-}
+} satisfies BookingConfirmationEmailProps
 
 export default BookingConfirmationEmail
