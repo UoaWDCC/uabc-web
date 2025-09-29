96bd6c5be8b40c12f183558fa11a23f9482a70bf
TheSKBroook <easonchang8@gmail.com>
2025-09-30 05:08:36 +1300

    fix: adding use client in semesterDatePopUp

--- packages/ui/src/components/Generic/CreateSemesterPopUpFlow/SemesterNamePopUp/SemesterNamePopUp.tsx
+++ packages/ui/src/components/Generic/CreateSemesterPopUpFlow/SemesterNamePopUp/SemesterNamePopUp.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { SemesterNamePopUpSchema, type SemesterNamePopUpValues } from "@repo/shared"
 import { Button, Heading, TextInput } from "@repo/ui/components/Primitive"
