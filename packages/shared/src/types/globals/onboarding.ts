effd20535a80910778f18ca692b1939c7f6469d6
mikais13 <mikai.somerville@gmail.com>
2025-10-04 15:03:39 +1300

    fix(types): use correct path to schemas in onboarding

--- packages/shared/src/types/globals/onboarding.ts
+++ packages/shared/src/types/globals/onboarding.ts
@@ -1,5 +1,5 @@
-import type { GetOnboardingResponseSchema, OnboardingGlobalSchema } from "src/schemas"
 import type { z } from "zod"
+import type { GetOnboardingResponseSchema, OnboardingGlobalSchema } from "../../schemas/globals"
 
 export type OnboardingGlobal = z.infer<typeof OnboardingGlobalSchema>
 export type GetOnboardingResponse = z.infer<typeof GetOnboardingResponseSchema>
