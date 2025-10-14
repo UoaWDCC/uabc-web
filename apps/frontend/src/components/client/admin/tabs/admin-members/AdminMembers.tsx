627bad445b8a525fb2568228243e4d88eb0dacc9
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-10-15 12:47:04 +1300

    fix: import useDeleteUser

--- apps/frontend/src/components/client/admin/tabs/admin-members/AdminMembers.tsx
+++ apps/frontend/src/components/client/admin/tabs/admin-members/AdminMembers.tsx
@@ -14,6 +14,7 @@ import { Dialog, useDisclosure, useNotice } from "@yamada-ui/react"
 import { useQueryState } from "nuqs"
 import {
   useCreateUser,
+  useDeleteUser,
   useResetAllMemberships,
   useUpdateUser,
 } from "@/services/admin/user/AdminUserMutations"
