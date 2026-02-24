878b32762b691b84a3b2b3078b34967c8cff3efe
xtncz <ascendus.dev@gmail.com>
2026-02-24 22:31:03 +1300

    refactor: make replyTo an array

--- apps/backend/src/business-layer/services/MailService.test.ts
+++ apps/backend/src/business-layer/services/MailService.test.ts
@@ -19,7 +19,7 @@ describe("MailService", () => {
 
       expect(sendEmailMock).toHaveBeenCalledWith({
         to: email,
-        replyTo: "badminton.au@gmail.com",
+        replyTo: ["badminton.au@gmail.com", "uabcbookings@gmail.com"],
         subject: "Email verification code",
         text: `Here is your email verification code: ${code}. This code will expire in 10 minutes.`,
       })
@@ -44,7 +44,7 @@ describe("MailService", () => {
 
       expect(sendEmailMock).toHaveBeenCalledWith({
         to: "straight.zhao@casual.com",
-        replyTo: "badminton.au@gmail.com",
+        replyTo: ["badminton.au@gmail.com", "uabcbookings@gmail.com"],
         subject: "UABC - Monday Booking Confirmation",
         html: expect.stringContaining(
           "Your booking for our Monday session at UoA Rec Center has been confirmed!",
@@ -74,7 +74,7 @@ describe("MailService", () => {
 
       expect(sendEmailMock).toHaveBeenCalledWith({
         to: "straight.zhao@casual.com",
-        replyTo: "badminton.au@gmail.com",
+        replyTo: ["badminton.au@gmail.com", "uabcbookings@gmail.com"],
         subject: "UABC - Monday Booking Confirmation",
         html: expect.any(String),
       })
@@ -101,7 +101,7 @@ describe("MailService", () => {
 
       expect(sendEmailMock).toHaveBeenCalledWith({
         to: "straight.zhao@casual.com",
-        replyTo: "badminton.au@gmail.com",
+        replyTo: ["badminton.au@gmail.com", "uabcbookings@gmail.com"],
         subject: `UABC - ${NOW.format("dddd")} Booking Confirmation`,
         html: expect.any(String),
       })
